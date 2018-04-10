1542757bc045453b9cf1cc3f02df432cff8cd648
Simon Perkins <simon.perkins@gmail.com>
2018-04-10 13:25:40 +0200

    Allow correlation shape to be specified
    dask array reshapes are potentially expensive, so encode
    the correlation shape appropriately.

--- tests/test_stokes.py
+++ tests/test_stokes.py
@@ -29,6 +29,9 @@ def test_brightness():
     B = brightness(stokes, polarisation_type='circular')
     assert np.all(B == expected)
 
+    B = brightness(stokes, polarisation_type='linear', corr_shape='matrix')
+    assert np.all(B==expected.reshape(2,2,1))
+
     # Four stokes parameters
     stokes = np.asarray([[1, 2, 3, 4], [5, 6, 7, 8]], dtype=np.float64)
 
@@ -44,6 +47,46 @@ def test_brightness():
                             [5+8,6+7*1j,6-7*1j,5-8]])
     assert np.all(B==expected)
 
+    # Test correlation shape
+    B = brightness(stokes, polarisation_type='linear', corr_shape='matrix')
+    expected = np.asarray([[1+2, 3+4*1j, 3-4*1j, 1-2],
+                            [5+6,7+8*1j,7-8*1j,5-6]]).reshape(2,2,2)
+    assert np.all(B==expected)
+
+
+def test_brightness_shape():
+    import numpy as np
+    from africanus.stokes import brightness
+
+    for pol_type in ('linear', 'circular'):
+        # 4 polarisation case
+        assert brightness(np.random.random((10,5,3,4)),
+            polarisation_type=pol_type,
+            corr_shape='matrix').shape == (10,5,3,2,2)
+
+        assert brightness(np.random.random((10,5,3,4)),
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,4)
+
+        # 2 polarisation case
+        assert brightness(np.random.random((10,5,3,2)),
+            polarisation_type=pol_type,
+            corr_shape='matrix').shape == (10,5,3,2,1)
+
+        assert brightness(np.random.random((10,5,3,2)),
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,2)
+
+        # 1 polarisation case
+        assert brightness(np.random.random((10,5,3,1)),
+            polarisation_type=pol_type,
+            corr_shape='matrix').shape == (10,5,3,1)
+
+        assert brightness(np.random.random((10,5,3,1)),
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,1)
+
+
 from africanus.stokes.dask import have_requirements
 
 @pytest.mark.skipif(not have_requirements, reason="requirements not installed")
@@ -53,13 +96,62 @@ def test_dask_brightness():
     from africanus.stokes.dask import brightness
 
     stokes = np.asarray([[1, 2, 3, 4], [5, 6, 7, 8]], dtype=np.float64)
+    da_stokes = da.from_array(stokes, (1,4))
 
-    B = brightness(da.from_array(stokes, (1,4)), polarisation_type='linear')
+    B = brightness(da_stokes, polarisation_type='linear')
     expected = np.asarray([[1+2, 3+4*1j, 3-4*1j, 1-2],
                             [5+6,7+8*1j,7-8*1j,5-6]])
     assert np.all(B.compute()==expected)
 
-    B = brightness(da.from_array(stokes, (1,4)), polarisation_type='circular')
+    # Test correlation shape
+    B = brightness(da_stokes, polarisation_type='linear', corr_shape='matrix')
+    assert np.all(B.compute()==expected.reshape(2,2,2))
+
+
+    B = brightness(da_stokes, polarisation_type='circular')
     expected = np.asarray([[1+4, 2+3*1j, 2-3*1j, 1-4],
                             [5+8,6+7*1j,6-7*1j,5-8]])
-    assert np.all(B.compute()==expected)
+
+    # Test correlation shape
+    B = brightness(da_stokes, polarisation_type='circular', corr_shape='matrix')
+    assert np.all(B.compute()==expected.reshape(2,2,2))
+
+
+@pytest.mark.skipif(not have_requirements, reason="requirements not installed")
+def test_dask_brightness_shape():
+    import dask.array as da
+    from africanus.stokes.dask import brightness
+
+    for pol_type in ('linear', 'circular'):
+        # 4 polarisation case
+        stokes = da.random.random((10,5,3,4),chunks=(5,(2,3),(2,1),4))
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='matrix').compute().shape == (10,5,3,2,2)
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,4)
+
+        # 2 polarisation case
+        stokes = da.random.random((10,5,3,2),chunks=(5,(2,3),(2,1),2))
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='matrix').shape == (10,5,3,2,1)
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,2)
+
+        # 1 polarisation case
+        stokes = da.random.random((10,5,3,1),chunks=(5,(2,3),(2,1),1))
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='matrix').shape == (10,5,3,1)
+
+        assert brightness(stokes,
+            polarisation_type=pol_type,
+            corr_shape='flat').shape == (10,5,3,1)
