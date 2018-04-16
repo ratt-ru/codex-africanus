3da5a9e64076ceab38d9efc6b1e5d2caa10ff48a
Simon Perkins <simon.perkins@gmail.com>
2018-04-16 11:50:27 +0200

    Support correlations in the last grid dimension
    Previously, the output of `grid` had shape `(fcorr, ny, nx)`
    where `fcorr` were the flattened correlations.
    Now, the output has been changed to `(ny, nx, corr_1, corr_2)`
    to be more inline with the RIME submodule.

--- africanus/gridding/simple/dask.py
+++ africanus/gridding/simple/dask.py
@@ -20,35 +20,47 @@ if not have_requirements or on_rtd():
 else:
     import numpy as np
     import dask.array as da
-    from .gridding import (grid as nb_grid_fn, degrid as nb_degrid_fn)
+    from .gridding import (grid as np_grid_fn, degrid as np_degrid_fn)
 
     def grid(vis, uvw, flags, weights, ref_wave,
              convolution_filter, nx=1024, ny=1024):
         """ Documentation below """
 
+        # Creation correlation dimension strings for each correlation
+        corrs = tuple('corr-%d' for i in range(len(vis.shape[2:])))
+
         # Unfortunately necessary to introduce an extra dim
         # for atop to work properly
-        def _grid_fn(*args, **kwargs):
-            return nb_grid_fn(*args, **kwargs)[None, :, :, :]
+        def _grid_fn(vis, uvw, flags, weights, ref_wave, convolution_filter):
+            return np_grid_fn(vis[0], uvw[0], flags[0], weights[0],
+                                ref_wave[0], convolution_filter,
+                                nx=nx, ny=ny)[None,:]
 
-        return da.core.atop(_grid_fn, ("row", "corr", "ny", "nx"),
-                            vis, ("row", "chan", "corr"),
+        # Get grids, stacked by row
+        grids = da.core.atop(_grid_fn, ("row", "ny", "nx") + corrs,
+                            vis, ("row", "chan") + corrs,
                             uvw, ("row", "(u,v,w)"),
-                            flags, ("row", "chan", "corr"),
-                            weights, ("row", "chan", "corr"),
+                            flags, ("row", "chan") + corrs,
+                            weights, ("row", "chan") + corrs,
                             ref_wave, ("chan",),
                             new_axes={"ny": ny, "nx": nx},
-                            adjust_chunks={"row": lambda n: 1},
-                            concatenate=True,
+                            adjust_chunks={"row": 1},
                             convolution_filter=convolution_filter,
-                            dtype=np.complex64).sum(axis=0)
+                            dtype=np.complex64)
+
+        # Sum grids over the row dimension to produce (ny, nx, corr_1, corr_2)
+        return grids.sum(axis=0)
 
     def degrid(grid, uvw, weights, ref_wave, convolution_filter):
         """ Documentation below """
-        return da.core.atop(nb_degrid_fn, ("row", "chan", "corr"),
-                            grid, ("corr", "ny", "nx"),
+
+        # Creation correlation dimension strings for each correlation
+        corrs = tuple('corr-%d' for i in range(len(grid.shape[2:])))
+
+        return da.core.atop(np_degrid_fn, ("row", "chan") + corrs,
+                            grid, ("ny", "nx") + corrs,
                             uvw, ("row", "(u,v,w)"),
-                            weights, ("row", "chan", "corr"),
+                            weights, ("row", "chan") + corrs,
                             ref_wave, ("chan",),
                             concatenate=True,
                             convolution_filter=convolution_filter,
@@ -63,20 +75,21 @@ resulting images are summed in a parallel reduction.
 Parameters
 ----------
 vis : :class:`dask.array.Array`
-    complex64 visibility array of shape (row, chan, corr)
+    complex64 visibility array of shape :code:`(row, chan, corr_1, corr_2)`
 uvw : np.ndarray
-    float64 array of UVW coordinates of shape (row, 3)
+    float64 array of UVW coordinates of shape :code:`(row, 3)`
 flags : :class:`dask.array.Array`
-    flagged data array of shape (row, chan, corr).
+    flagged data array of shape :code:`(row, chan, corr_1, corr_2)`.
     Any positive quantity will indicate that the corresponding
     visibility should be flagged.
     Set this to ``dask.array.zeros_like(vis, dtype=np.bool)``
     as default.
 weights : :class:`dask.array.Array`
-    float32 or float64 array of weights. Set this to
+    float32 or float64 array of weights of shape :code:`(row, chan, corr_1, corr_2)`.
+    Set this to
     ``dask.array.ones_like(vis, dtype=np.float32)`` as default.
 ref_wave : :class:`dask.array.Array`
-    float64 array of wavelengths of shape (chan,)
+    float64 array of wavelengths of shape :code:`(chan,)`
 convolution_filter : :class:`~africanus.filters.ConvolutionFilter`
     Convolution filter
 nx : integer, optional
@@ -87,7 +100,7 @@ ny : integer, optional
 Returns
 -------
 :class:`dask.array.Array`
-    (corr, ny, nx) complex64 array of gridded visibilities
+    (ny, nx, corr_1, corr_2) complex64 array of gridded visibilities
     constructed by summing convolutional weights.
 """
 
@@ -98,19 +111,20 @@ Parameters
 ----------
 grid : :class:`dask.array.Array`
     float or complex grid of visibilities
-    of shape (corr, ny, nx)
+    of shape :code:`(ny, nx, corr_1, corr_2)`
 uvw : :class:`dask.array.Array`
-    float64 array of UVW coordinates of shape (row, 3)
+    float64 array of UVW coordinates of shape :code:`(row, 3)`
 weights : :class:`dask.array.Array`
-    float32 or float64 array of weights. Set this to
+    float32 or float64 array of weights of shape
+    :code:`(row, chan, corr_1, corr_2)`. Set this to
     ``dask.array.ones_like(vis, dtype=np.float32)`` as default.
 ref_wave : :class:`dask.array.Array`
-    float64 array of wavelengths of shape (chan,)
+    float64 array of wavelengths of shape :code:`(chan,)`
 convolution_filter :  :class:`~africanus.filters.ConvolutionFilter`
     Convolution Filter
 
 Returns
 -------
 :class:`dask.array.Array`
-    (chan, corr) complex64 visibilities
+    :code:`(row, chan, corr_1, corr_2)` complex64 visibilities
 """
