ea8e87103066e8c6e33423bdb156c9962bcfd83a
landmanbester <lbester@ska.ac.za>
2018-04-24 10:53:40 +0200

    Fixed some latex in docs that pep8 wasn't happy with

--- africanus/dft/kernels.py
+++ africanus/dft/kernels.py
@@ -90,7 +90,7 @@ im_to_vis_docs = _DFT_DOCSTRING(
 
     .. math::
 
-        & {\\Large \sum_s e^{-2 \\pi i (u l_s + v m_s + w (n_s - 1)} \cdot I_s }
+        & {\\Large \\sum_s e^{-2 \\pi i (u l_s + v m_s + w (n_s - 1)} \\cdot I_s }
 
     """,  # noqa
 
@@ -132,7 +132,7 @@ vis_to_im_docs = _DFT_DOCSTRING(
 
     .. math::
 
-        & {\\Large \sum_n exp{ 2 \\pi i (u_n l + v_n m + w_n (n - 1))} \cdot V_n
+        & {\\Large \\sum_n exp{ 2 \\pi i (u_n l + v_n m + w_n (n - 1))} \\cdot V_n
 
     """,  # noqa
 
