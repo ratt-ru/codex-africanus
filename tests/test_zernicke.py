22d635924911b85e1d83a231b56b53c12b16f55d
Joshua <g14v2805@campus.ru.ac.za>
2018-05-16 15:02:01 +0200

    Tried generating a graph for the Zernike polynomials.

--- tests/test_zernicke.py
+++ tests/test_zernicke.py
@@ -32,4 +32,42 @@ def test_zernike_dde():
     zernike_value = zernike_dde(coords, coeffs, noll_indices)
     assert zernike_value.shape == (nsrc, ntime, na, nchan)
 
-test_zernike_dde()
+def test_zernike_func():
+    from africanus.rime import zernike_dde
+    Ll = np.deg2rad(3)
+    Lm = np.deg2rad(3)
+    j = 1
+    nsrc = 10000
+    ntime = 1
+    npoly = 1
+    na = 1
+    nchan = 1
+
+    # Linear (l,m) grid
+    l = np.linspace(-Ll, Ll, nsrc)
+    m = np.linspace(-Lm, Lm, nsrc)
+    
+    # Initializing coords, coeffs, and noll_indices
+    # coords has nsrc elements (the (l,m) grid), coeffs holds the value 1, and noll_indices has a single value j 
+    coords = np.empty((3, nsrc, ntime, na, nchan), dtype=np.float)
+    coeffs = np.empty((na, nchan, npoly))
+    coeffs[0, 0, 0] = 1
+    noll_indices = np.empty((na, nchan, npoly))
+    noll_indices[0,0,0] = j
+
+    # I left 0 as all the freq values
+    for s in range(nsrc):
+        coords[0, s, 0, 0, 0] = l[s]
+        coords[1, s, 0, 0, 0] = m[s]
+        coords[2, s, 0, 0, 0] = 0
+    zernike_vals = zernike_dde(coords, coeffs, noll_indices)[:, 0, 0, 0].reshape((int(np.sqrt(nsrc)), int(np.sqrt(nsrc))))
+
+    plt.figure()
+    plt.title(r'$Z_{%d}(l,m)$' %j)
+    plt.imshow(np.real(zernike_vals))
+    plt.colorbar()
+    plt.savefig('./zernicke_test_figs/nsrc_10000_ntime_1_npoly_1_na_1_nchan_1.png')
+
+
+    
+test_zernike_func()
