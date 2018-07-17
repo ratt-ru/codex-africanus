980e37d1636bcbfaf5c095824eacb0c62352e4b2
caramel-koala <antoniob.peters@gmail.com>
2018-07-17 17:22:07 +0200

    Forgot to add the test update

--- tests/test_opts.py
+++ tests/test_opts.py
@@ -4,55 +4,104 @@
 """Tests for `codex-africanus` package."""
 
 import numpy as np
-from africanus.dft.dask import im_to_vis, vis_to_im
+#from africanus.dft.dask import im_to_vis, vis_to_im
+from africanus.dft.kernels import im_to_vis, vis_to_im
 from casacore.tables import table
 from astropy.io import fits
 import dask.array as da
 import pytest
+import xarrayms
+import matplotlib.pyplot as plt
+from africanus.opts.primaldual import primal_dual_solver as pds
+from africanus.opts.sub_opts import pow_method
 
 # def test_pd():
 """
 Tests to see if there is any great difference between the model image image and the resolved image
 """
-with fits.open("/home/antonio/Downloads/WSCMSSSMFTestSuite/output.fits") as the_file:
 
-    model = the_file[0]
-    # generate lm-coordinates
-    Npix_h = model.data.shape[2]
-    Npix_v = model.data.shape[3]
-    cdelt_r = model.header['CDELT1']
-    cdelt_d = model.header['CDELT2']
-    ra = np.linspace((Npix_h/2)*cdelt_r, -(Npix_h/2)*cdelt_r, Npix_h)
-    ra_delta = ra - ra[0]
-    dec = np.linspace(-(Npix_v/2)*cdelt_r, (Npix_v/2)*cdelt_r, Npix_v)
-    l = np.cos(dec) * np.sin(ra_delta)
-    m = (np.sin(dec) * np.cos(dec[0]) - np.cos(dec) * np.sin(dec[0]) * np.cos(ra_delta))
-    ll, mm = np.meshgrid(l, m)
-    lm = np.vstack((ll.flatten(), mm.flatten())).T
-    print(lm.shape)
+npix = 256
 
-    # generate frequencies
-    base_freq = model.header['CRVAL4']
-    cdelt_f = model.header['CDELT4']
-    Npix_f = 8
-    freq = np.array([base_freq])#.linspace(base_freq, base_freq + Npix_f*cdelt_f, Npix_f)
-    #print(freq.shape)
+# generate lm-coordinates
+xmin = -0.005
+xmax = 0.005
+x = np.linspace(xmin, xmax, npix)
+ll, mm = np.meshgrid(x, x)
+lm = np.vstack((ll.flatten(), mm.flatten())).T
 
-    # make sky model for calibration
-    t = table("/home/antonio/Downloads/WSCMSSSMFTestSuite/SSMF.MS_p0", readonly=True)
-    vis = t.getcol("DATA")[:, 0:1, 0]
-    print(vis.shape)
-    uvw = t.getcol("UVW")
-    print(uvw.shape)
-    weight = t.getcol("WEIGHT")
-    weight = np.ones_like(weight)
+# generate frequencies
+freq = np.array([1.48e9])#.linspace(base_freq, base_freq + Npix_f*cdelt_f, Npix_f)
+#print(freq.shape)
 
-    # set up dask arrays
-    uvw_dask = da.from_array(uvw, chunks=(25, 3))
-    lm_dask = da.from_array(lm, chunks=(25, 2))
-    frequency_dask = da.from_array(freq, chunks=1)
-    vis_dask = da.from_array(vis, chunks=(25, 1))
+data_path = "/home/antonio/Downloads/WSCMSSSMFTestSuite/SSMF.MS_p0"
 
-    dirty = vis_to_im(vis_dask, uvw_dask, lm_dask, frequency_dask, np.complex64).compute()
+nrow = 500
+nchan = 1
 
-    print(dirty)
+# make sky model for calibration
+# t = table(data_path, readonly=True)
+# Vdat = t.getcol("DATA")[:, 0:1, 0]
+# uvw = t.getcol("UVW")
+# weight = t.getcol("WEIGHT")
+# weight = np.ones_like(weight)
+
+for ds in xarrayms.xds_from_ms(data_path):
+    Vdat = ds.DATA.data.compute()
+    uvw = ds.UVW.data.compute()[0:nrow,:]
+    weights = ds.WEIGHT.data.compute()[0:nrow, 0:nchan]
+
+vis = ((Vdat[0:nrow, 0:nchan, 0] + Vdat[0:nrow, 0:nchan, 0])/2.0).reshape(nrow, nchan)
+
+# set up dask arrays
+uvw_dask = da.from_array(uvw, chunks=(1000, 3))
+lm_dask = da.from_array(lm, chunks=(npix, 2))
+frequency_dask = da.from_array(freq, chunks=nchan)
+vis_dask = da.from_array(vis, chunks=(1000, nchan))
+weights_dask = da.from_array(weights, chunks=(1000, nchan))
+
+# PSF = vis_to_im(weights_dask, uvw_dask, lm_dask, frequency_dask).compute()
+PSF = vis_to_im(weights, uvw, lm, freq)
+
+wsum = PSF.max()
+
+
+def L(image):
+    return im_to_vis(image, uvw, lm, freq)/wsum
+
+
+def LT(v):
+    return vis_to_im(v, uvw, lm, freq)/wsum
+
+# dirty = vis_to_im(vis_dask, uvw_dask, lm_dask, frequency_dask).compute()/wsum
+dirty = LT(vis)
+
+test = pow_method(L, LT, [npix, npix])
+print(test)
+# dirty_dask = da.from_array(dirty, chunks=npix)
+
+cleaned = pds(dirty, vis, L, LT)
+
+plt.figure('PSF')
+plt.imshow(PSF.reshape(npix, npix)/wsum)
+plt.colorbar()
+
+
+plt.figure('ID')
+plt.imshow(dirty.reshape(npix, npix))
+plt.colorbar()
+plt.show()
+
+plt.figure('IM')
+plt.imshow(cleaned.reshape(npix, npix))
+plt.colorbar()
+plt.show()
+
+hdu = fits.PrimaryHDU(dirty.reshape(npix, npix))
+hdul = fits.HDUList([hdu])
+hdul.writeto('/home/antonio/Downloads/WSCMSSSMFTestSuite/dirty.fits', overwrite=True)
+hdul.close()
+
+hdu = fits.PrimaryHDU(cleaned.reshape(npix, npix))
+hdul = fits.HDUList([hdu])
+hdul.writeto('/home/antonio/Downloads/WSCMSSSMFTestSuite/recovered.fits', overwrite=True)
+hdul.close()
