8011a2aab5baafbc92ebc2288babf080e56ebf4d
Simon Perkins <simon.perkins@gmail.com>
2018-07-31 14:52:49 +0100

    Test case

---
+++ tests/test_w_projection.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def test_polyfit2d():
+    from africanus.filters.wproj.spheroidal import polyfit2d, polyval2d
+
+    npoints = 10
+    x = np.random.random(npoints)
+    y = np.random.random(npoints)
+    z = x**2 + y**2 + 3*x**3 + y + np.random.random(npoints)
+    coeffs = polyfit2d(x,y,z,order=5)
+    rz = polyval2d(x,y,coeffs)
+
+    assert np.allclose(z, rz)
