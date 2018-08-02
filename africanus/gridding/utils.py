73305ca525e344c86c5835675e5e4be44728c580
Simon Perkins <simon.perkins@gmail.com>
2018-08-02 11:06:41 +0100

    Add uv scaling function

--- africanus/gridding/utils.py
+++ africanus/gridding/utils.py
@@ -30,3 +30,32 @@ def cell_size(nl, nm, umax, vmax):
         Cell size in the l and m dimensions
     """
     return np.array([umax / (2.*nl), vmax / (2.*nm)])
+
+
+_ARCSEC2RAD = np.deg2rad(1.0/(60*60))
+
+
+def uv_scale(nl, nm, umax, vmax):
+    r"""
+    Returns the UV scaling parameters given
+    the maximum ``U`` and ``V`` coordinate.
+
+    Parameters
+    ----------
+    nl : int
+        Number of pixels in ``l``
+    nm : int
+        Number of pixels in ``m``
+    umax : float
+        Maximum U coordinate
+    vmax : float
+        Maximum V coordinate
+
+
+    Returns
+    -------
+    tuple
+        Cell size in the l and m dimensions
+    """
+
+    return _ARCSEC2RAD * cell_size(nl, nm, umax, vmax)
