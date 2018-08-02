Correct w-layer calculation
This is still incorrect though

africanus/gridding/wstack/wstacking.py
@@ -38,9 +38,8 @@ def w_stacking_layers(w_min, w_max, l, m):
     int
         Number of w-layers
     """
-    max_n = np.sqrt(1 - l[None, :]**2 - m[:, None]**2).max()
-
-    return np.ceil(2*np.pi*(w_max - w_min)/max_n).astype(np.int32).item()
+    max_val = (1.0 - np.sqrt(1 - l[None, :]**2 - m[:, None]**2)).max()
+    return np.ceil(2*np.pi*(w_max - w_min)/max_val).astype(np.int32).item()
 
 
 def w_stacking_bins(w_min, w_max, w_layers):