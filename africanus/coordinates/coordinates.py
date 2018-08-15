75df903288b2a1279d24eff0a18db5ca524a09f7
Simon Perkins <simon.perkins@gmail.com>
2018-08-15 09:21:24 +0200

    docstring fixes

--- africanus/coordinates/coordinates.py
+++ africanus/coordinates/coordinates.py
@@ -32,26 +32,6 @@ def _nb_radec_to_lmn(radec, phase_centre):
     return lmn
 
 
-@numba.jit(nopython=True, nogil=True, cache=True)
-def _nb_lm_to_radec(lmn, phase_centre):
-    assert lmn.ndim == 2 and lmn.shape[1] == 3
-    assert phase_centre.ndim == 1 and phase_centre.shape[0] == 2
-
-    radec = np.empty(shape=(lmn.shape[0], 2), dtype=lmn.dtype)
-
-    pc_ra, pc_dec = phase_centre
-    sin_d0 = np.sin(pc_dec)
-    cos_d0 = np.cos(pc_dec)
-
-    for s in range(radec.shape[0]):
-        l, m, n = lmn[s]
-
-        radec[s, 1] = np.arcsin(m*cos_d0 + n*sin_d0)
-        radec[s, 0] = pc_ra + np.arctan(l / (n*cos_d0 - m*sin_d0))
-
-    return radec
-
-
 def radec_to_lmn(radec, phase_centre=None):
     r"""
     Converts Right-Ascension/Declination coordinates in radians
@@ -92,6 +72,26 @@ def radec_to_lmn(radec, phase_centre=None):
     return _nb_radec_to_lmn(radec, phase_centre)
 
 
+@numba.jit(nopython=True, nogil=True, cache=True)
+def _nb_lm_to_radec(lmn, phase_centre):
+    assert lmn.ndim == 2 and lmn.shape[1] == 3
+    assert phase_centre.ndim == 1 and phase_centre.shape[0] == 2
+
+    radec = np.empty(shape=(lmn.shape[0], 2), dtype=lmn.dtype)
+
+    pc_ra, pc_dec = phase_centre
+    sin_d0 = np.sin(pc_dec)
+    cos_d0 = np.cos(pc_dec)
+
+    for s in range(radec.shape[0]):
+        l, m, n = lmn[s]
+
+        radec[s, 1] = np.arcsin(m*cos_d0 + n*sin_d0)
+        radec[s, 0] = pc_ra + np.arctan(l / (n*cos_d0 - m*sin_d0))
+
+    return radec
+
+
 def lmn_to_radec(lmn, phase_centre=None):
     r"""
     Convert Direction Cosine lm coordinates to Right Ascension/Declination
@@ -101,8 +101,10 @@ def lmn_to_radec(lmn, phase_centre=None):
         :nowrap:
 
         \begin{eqnarray}
-        & \delta = \, \arcsin \( m \cos \delta 0 + n \sin \delta 0 \)
-        & \alpha = \, \arctan \( \frac{l, n \cos \delta 0 - m \sin \delta 0} \)
+        & \delta = & \, \arcsin \left( m \cos \delta 0 +
+                                     n \sin \delta 0 \right) \\
+        & \alpha = & \, \arctan \left( \frac{l}{n \cos \delta 0 -
+                                     m \sin \delta 0} \right) \\
         \end{eqnarray}
 
     where :math:`\alpha` is the Right Ascension of each coordinate
