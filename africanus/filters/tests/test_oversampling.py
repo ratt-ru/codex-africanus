441893744ce2a50ee9c85271705a95c893d7a12b
Simon Perkins <simon.perkins@gmail.com>
2018-11-06 16:38:49 +0200

    Update oversampling test case

--- africanus/filters/tests/test_oversampling.py
+++ africanus/filters/tests/test_oversampling.py
@@ -12,7 +12,7 @@ from africanus.filters.kaiser_bessel_filter import kaiser_bessel_with_sinc
 from africanus.gridding.simple.gridding import _ARCSEC2RAD
 
 
-@pytest.mark.parametrize("exact_u", [np.array([-1.2])])
+@pytest.mark.parametrize("exact_u", [np.array([1.9])])
 @pytest.mark.parametrize("full_support, oversampling, beta", [[7, 7, 2.4]])
 @pytest.mark.parametrize("ref_wave", [c / (0.5*(.856e9 + 2*.856e9))])
 @pytest.mark.parametrize("nx", [16])
@@ -21,14 +21,15 @@ from africanus.gridding.simple.gridding import _ARCSEC2RAD
 def test_oversampling(exact_u, full_support, oversampling, beta,
                       ref_wave, nx, cell_size, plot):
     W = full_support*oversampling
+    half_support = full_support // 2
     half_x = nx // 2
 
     exact_u += half_x
-    os_disc_u = np.round(exact_u).astype(np.int32)
+    disc_u = np.round(exact_u).astype(np.int32)
 
-    if np.any(os_disc_u <= 0) or np.any(os_disc_u > nx):
+    if np.any(disc_u <= 0) or np.any(disc_u > nx):
         raise ValueError("Pick values inside the grid [%s-%s]\n%s"
-                         % (0, nx, os_disc_u))
+                         % (0, nx, disc_u))
 
     conv_filter = kaiser_bessel_with_sinc(full_support,
                                           oversampling,
@@ -44,17 +45,19 @@ def test_oversampling(exact_u, full_support, oversampling, beta,
 
     # We snap to the oversampling index base on the fractional
     # value of the u and v coordinate
-    if os_disc_u > exact_u:
-        base_frac_u = exact_u - os_disc_u + 1.0
-    else:
-        base_frac_u = exact_u - os_disc_u
+    # if disc_u > exact_u:
+    #     base_frac_u = exact_u - disc_u + 1.0
+    # else:
+    #     base_frac_u = exact_u - disc_u
 
-    assert np.all(base_frac_u == exact_u - np.floor(exact_u).astype(np.int32))
+    base_frac_u = exact_u - disc_u
 
     # Calculate oversampling index and normalise it
     base_os_u = np.round(base_frac_u*oversampling).astype(np.int32)
+    base_os_u = (base_os_u + (3*oversampling)//2) % oversampling
+    # base_os_u = (base_os_u + oversampling) % oversampling
 
-    print(exact_u, os_disc_u, base_frac_u)
+    print(exact_u, disc_u, base_frac_u, base_os_u)
 
     if plot:
         try:
@@ -67,22 +70,24 @@ def test_oversampling(exact_u, full_support, oversampling, beta,
             f.set_figwidth(80)
             f.set_figheight(60)
             axes[0].scatter(exact_u, np.ones_like(exact_u)*0.25, color='blue')
-            axes[0].scatter(os_disc_u, np.ones_like(exact_u)*0.1, color='red')
+            axes[0].scatter(disc_u, np.ones_like(exact_u)*0.1, color='red')
             axes[0].set_xticks(x)
             axes[0].set_ylim(0, 0.5)
             axes[0].set_xlabel("grid position")
 
-            half_support = full_support // 2
             x = np.linspace(0, full_support, conv_filter.size)
             vl = np.linspace(0, full_support, conv_filter.size // oversampling)
-            # pts = (vl + os_disc_u - exact_u)[:-1]
+            # pts = (vl + disc_u - exact_u)[:-1]
             # axes[1, 0].scatter(pts, np.ones_like(pts)*0)
-            pts = (base_frac_u + vl)[:-1]
 
+            pts = (base_frac_u + vl[:-1])
             axes[1].plot(x, conv_filter)
             axes[1].vlines(vl, ymin=conv_filter.min(),
                            ymax=0.1 * conv_filter.max())
-            axes[1].scatter(pts, np.ones_like(pts)*0)
+            axes[1].scatter(pts, np.ones_like(pts)*0, color='blue')
             axes[1].set_xlabel("filter position")
+
+            pts = (base_os_u/oversampling + vl)[:-1]
+            axes[1].scatter(pts, np.ones_like(pts)*0, color='red')
 
             plt.show()
