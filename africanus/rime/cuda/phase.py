ac8757f179d8f98008a30df16f3542fec51e4b52
Simon Perkins <simon.perkins@gmail.com>
2018-11-29 22:22:49 +0200

    Use jinja2 templating

--- africanus/rime/cuda/phase.py
+++ africanus/rime/cuda/phase.py
@@ -19,23 +19,28 @@ try:
 except ImportError:
     pass
 
+try:
+    from jinja2 import Template
+except ImportError:
+    pass
+
 log = logging.getLogger(__name__)
 
-code = Template("""
+_PHASE_DELAY_TEMPLATE = r"""
 // #include <cupy/complex.cuh>
 #include <cupy/carray.cuh>
 // #include <cupy/atomics.cuh>
 
-#define blockdimx ${blockdimx}
-#define blockdimy ${blockdimy}
+#define blockdimx {{blockdimx}}
+#define blockdimy {{blockdimy}}
 
-#define minus_two_pi_over_c ${minus_two_pi_over_c}
+#define minus_two_pi_over_c {{minus_two_pi_over_c}}
 
-extern "C" __global__ void ${kernel_name}(
-    const CArray<${lm_type}, 2> lm,
-    const CArray<${uvw_type}, 2> uvw,
-    const CArray<${freq_type}, 1> frequency,
-    CArray<${out_type}2, 3> complex_phase)
+extern "C" __global__ void {{kernel_name}}(
+    const CArray<{{lm_type}}, 2> lm,
+    const CArray<{{uvw_type}}, 2> uvw,
+    const CArray<{{freq_type}}, 1> frequency,
+    CArray<{{out_type}}2, 3> complex_phase)
 {
     int row = blockIdx.y*blockDim.y + threadIdx.y;
     int chan = blockIdx.x*blockDim.x + threadIdx.x;
@@ -45,14 +50,14 @@ extern "C" __global__ void ${kernel_name}(
         { return; }
 
     // Reinterpret inputs as vector types
-    const ${lm_type}2 * lm_ptr = reinterpret_cast<const ${lm_type}2 *>(&lm[0]);
-    const ${uvw_type}3 * uvw_ptr = reinterpret_cast<const ${uvw_type}3 *>(&uvw[0]);
-    ${out_type}2 * complex_phase_ptr = reinterpret_cast<${out_type}2 *>(
+    const {{lm_type}}2 * lm_ptr = reinterpret_cast<const {{lm_type}}2 *>(&lm[0]);
+    const {{uvw_type}}3 * uvw_ptr = reinterpret_cast<const {{uvw_type}}3 *>(&uvw[0]);
+    {{out_type}}2 * complex_phase_ptr = reinterpret_cast<{{out_type}}2 *>(
                                                     &complex_phase[0]);
 
     __shared__ struct {
-        ${uvw_type}3 uvw[blockdimy];
-        ${freq_type} frequency[blockdimx];
+        {{uvw_type}}3 uvw[blockdimy];
+        {{freq_type}} frequency[blockdimx];
     } shared;
 
     // UVW coordinates vary along y dimension only
@@ -67,9 +72,9 @@ extern "C" __global__ void ${kernel_name}(
 
     for(int source = 0; source < lm.shape()[0]; ++source)
     {
-        ${lm_type}2 rlm = lm_ptr[source];
-        ${lm_type} n = ${sqrt_fn}(1.0 - rlm.x*rlm.x - rlm.y*rlm.y) - 1.0;
-        ${out_type} real_phase = rlm.x*shared.uvw[threadIdx.y].x +
+        {{lm_type}}2 rlm = lm_ptr[source];
+        {{lm_type}} n = {{sqrt_fn}}(1.0 - rlm.x*rlm.x - rlm.y*rlm.y) - 1.0;
+        {{out_type}} real_phase = rlm.x*shared.uvw[threadIdx.y].x +
                                  rlm.y*shared.uvw[threadIdx.y].y +
                                  n*shared.uvw[threadIdx.y].z;
 
@@ -78,46 +83,48 @@ extern "C" __global__ void ${kernel_name}(
                      real_phase *
                      shared.frequency[threadIdx.x];
 
-        ${out_type}2 cplx_phase;
-        ${sincos_fn}(real_phase, &cplx_phase.y, &cplx_phase.x);
+        {{out_type}}2 cplx_phase;
+        {{sincos_fn}}(real_phase, &cplx_phase.y, &cplx_phase.x);
 
 
         ptrdiff_t idx [] = {source, row, chan};
         complex_phase[idx] = cplx_phase;
     }
 }
-""")
+"""
 
 
 class PhaseDelayKernel(object):
-    def __init__(self, name, code_template):
+    @requires_optional('jinja2')
+    def __init__(self, name):
         self._name = name
-        self._code_template = code_template
+        self._code_template = Template(_PHASE_DELAY_TEMPLATE)
         self._arg_names = ("lm", "uvw", "frequency")
 
-    def generate_kernel(self, lm, uvw, frequency, out_dtype):
+    def _generate_kernel(self, lm, uvw, frequency, out_dtype):
         blockdimx = 32 if frequency.dtype == np.float32 else 16
         blockdimy = 32 if uvw.dtype == np.float32 else 16
-        substitute = self._code_template.substitute
+        render = self._code_template.render
 
-        code = substitute(kernel_name=self._name,
-                          lm_type=_get_typename(lm.dtype),
-                          uvw_type=_get_typename(uvw.dtype),
-                          freq_type=_get_typename(frequency.dtype),
-                          out_type=_get_typename(out_dtype),
-                          sqrt_fn=cuda_function('sqrt', lm.dtype),
-                          sincos_fn=cuda_function('sincos', out_dtype),
-                          minus_two_pi_over_c=minus_two_pi_over_c,
-                          blockdimx=blockdimx,
-                          blockdimy=blockdimy)
+        code = render(kernel_name=self._name,
+                      lm_type=_get_typename(lm.dtype),
+                      uvw_type=_get_typename(uvw.dtype),
+                      freq_type=_get_typename(frequency.dtype),
+                      out_type=_get_typename(out_dtype),
+                      sqrt_fn=cuda_function('sqrt', lm.dtype),
+                      sincos_fn=cuda_function('sincos', out_dtype),
+                      minus_two_pi_over_c=minus_two_pi_over_c,
+                      blockdimx=blockdimx,
+                      blockdimy=blockdimy).encode('utf-8')
 
         block = (blockdimx, blockdimy, 1)
 
         return cp.RawKernel(code, self._name), block, code
 
+    @requires_optional('cupy', 'jinja2')
     def __call__(self, lm, uvw, frequency):
         out_dtype = np.result_type(lm, uvw, frequency)
-        kernel, block, code = self.generate_kernel(lm, uvw, frequency,
+        kernel, block, code = self._generate_kernel(lm, uvw, frequency,
                                                    out_dtype)
 
         grid = grids((frequency.shape[0], uvw.shape[0], 1), block)
@@ -134,4 +141,4 @@ class PhaseDelayKernel(object):
         return out
 
 
-phase_delay = PhaseDelayKernel("phase_delay", code)
+phase_delay = PhaseDelayKernel("phase_delay")
