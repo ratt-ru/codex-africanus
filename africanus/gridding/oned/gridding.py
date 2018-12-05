f65ae16db0167be85896a5776cdef465d8294154
Simon Perkins <simon.perkins@gmail.com>
2018-12-05 15:22:37 +0200

    Further WIP

--- africanus/gridding/oned/gridding.py
+++ africanus/gridding/oned/gridding.py
@@ -11,12 +11,19 @@ import numpy as np
 _ARCSEC2RAD = np.deg2rad(1.0/(60*60))
 
 
+nearest_neighbour = False
+
+
 def grid(vis, uvw, ref_wave, conv_filter, oversample,
          cell_size, grid):
 
     nx = grid.shape[0]
-    half_x = nx // 2
+
+    assert nx % 2 == 1
+    assert conv_filter.shape[0] % 2 == 1
+
     half_support = (conv_filter.shape[0] // oversample) // 2
+    centre_x = nx // 2
 
     # Similarity Theorem
     # https://www.cv.nrao.edu/course/astr534/FTSimilarity.html
@@ -28,19 +35,28 @@ def grid(vis, uvw, ref_wave, conv_filter, oversample,
         scaled_u = uvw[r, 0] * u_scale
 
         for f in range(vis.shape[1]):
-            exact_u = half_x + (scaled_u / ref_wave[f])
+            exact_u = centre_x + (scaled_u / ref_wave[f])
             disc_u = int(np.round(exact_u))
 
             frac_u = exact_u - disc_u
             base_os_u = int(np.round(frac_u*oversample))
+            saved = base_os_u
             base_os_u = (base_os_u + ((3 * oversample) // 2)) % oversample
 
-            lower_u = disc_u - half_support      # Inclusive
-            upper_u = disc_u + half_support + 1  # Exclusive
+            lower_u = disc_u - half_support               # Inclusive
+            upper_u = disc_u + half_support + 1           # Exclusive
+
+            print("\n")
+            print("exact %.3f discrete %.3f frac_u*os %.3f frac %.3f "
+                  "os_u %.3f lower_u %.3f upper_u %.3f "
+                  % (exact_u, disc_u, frac_u*oversample,
+                     frac_u, base_os_u, lower_u, upper_u))
 
             print(exact_u, disc_u, lower_u, upper_u)
 
-            for ui, grid_u in enumerate(range(lower_u, upper_u)):
-                conv_weight = conv_filter[base_os_u + ui*oversample]
-
-                grid[grid_u] += vis[r, f] * conv_weight
+            if nearest_neighbour is True:
+                grid[disc_u] += vis[r, f]
+            else:
+                for ui, grid_u in enumerate(range(lower_u, upper_u)):
+                    conv_weight = conv_filter[base_os_u + ui*oversample]
+                    grid[grid_u] += vis[r, f] * conv_weight
--- africanus/gridding/oned/tests/test_gridding.py
+++ africanus/gridding/oned/tests/test_gridding.py
@@ -5,6 +5,37 @@ import pytest
 from africanus.constants import c as lightspeed
 from africanus.constants import minus_two_pi_over_c
 
+from africanus.gridding.oned.gridding import grid
+from africanus.filters.kaiser_bessel_filter import (
+                    kaiser_bessel,
+                    kaiser_bessel_fourier,
+                    kaiser_bessel_with_sinc)
+
+from scipy.fftpack import ifft, fftshift, ifftshift
+
+
+def kaiser_bessel_taper(full_support, oversample, beta, nx):
+        kb = kaiser_bessel_with_sinc(full_support, oversample, beta)
+        kbshift = np.fft.fftshift(kb)
+
+        width = nx * oversample
+
+        # Put the first and last halves of the shifted Kaiser Bessel
+        # at each end of the output buffer, then FFT
+        buf = np.zeros(width, dtype=kb.dtype)
+        buf[:kbshift.size // 2] = kbshift[:kbshift.size // 2]
+        buf[-kbshift.size // 2:] = kbshift[-kbshift.size // 2:]
+
+        x = np.fft.ifft(buf).real
+
+        # First half of the taper
+        half = x[:1 + (nx // 2)]
+
+        taper = np.empty(nx, dtype=kb.dtype)
+        taper[:nx // 2] = half[:-1]
+        taper[nx // 2:] = half[::-1]
+        return taper*oversample
+
 
 def vis_to_im_impl(vis, uvw, lm, frequency, im_of_vis):
     # For each source
@@ -32,45 +63,64 @@ def vis_to_im_impl(vis, uvw, lm, frequency, im_of_vis):
 
 
 def test_oned_gridding():
-    from africanus.gridding.oned.gridding import grid
-    from africanus.filters.kaiser_bessel_filter import (
-                        kaiser_bessel,
-                        kaiser_bessel_fourier)
-
     _ARCSEC2RAD = np.deg2rad(1.0/(60*60))
-    filter_width = 7
-    oversample = 1
-    beta = 2.34
+    filter_width = 15
+    oversample = 7
+    beta = 2.34*filter_width
     cell_size = 8.
-    grid_size = 21
     cell_size_rad = _ARCSEC2RAD*cell_size
+    nx = 65
 
-    vis = np.asarray([[1.0 + 2.0j]], dtype=np.complex128)
+    vis = np.asarray([[1.0 + 0.0j]], dtype=np.complex128)
     uvw = np.asarray([[10000.0]], dtype=np.float64)
     freq = np.asarray([.856e9], dtype=np.float64)
     ref_wave = freq / lightspeed
     width = filter_width*oversample
 
     u = np.arange(width, dtype=np.float64) - width // 2
-
-    conv_filter = kaiser_bessel(u, width, beta)
-    oned_grid = np.empty(grid_size, dtype=np.complex128)
+    conv_filter = kaiser_bessel_with_sinc(filter_width, oversample, beta)
+    oned_grid = np.zeros(nx, dtype=np.complex128)
 
     grid(vis, uvw, ref_wave, conv_filter, oversample, cell_size, oned_grid)
 
-    lm_extents = cell_size_rad*(grid_size // 2)
-    lm = np.linspace(-lm_extents, lm_extents, grid_size)
+    # FFT and normalise
+    oned_grid_fft = fftshift(ifft(ifftshift(oned_grid)))
+    oned_grid_fft *= nx
+
+    # Apply the taper
+    taper = kaiser_bessel_taper(filter_width, oversample, beta, nx)
+    oned_grid_fft /= taper
+
+    # Do the DFT
+    lm = np.arange(nx, dtype=np.float64) - (nx // 2)
+    lm *= cell_size_rad
+    print("lm", lm)
+    # Centre point must be 0.0
+    assert lm[nx // 2] == 0.0
+
     dft_grid = np.zeros((oned_grid.shape[0], freq.shape[0]),
                         dtype=oned_grid.dtype)
 
     vis_to_im_impl(vis, uvw, lm, freq, dft_grid)
-    oned_grid_fft = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(oned_grid)))
-    oned_grid_fft *= grid_size
 
-    u = (np.arange(grid_size, dtype=np.float64) - (grid_size // 2)) / grid_size
-    taper = kaiser_bessel_fourier(u, filter_width, beta*filter_width)
-    oned_grid_fft /= taper
+    np_vis = np.exp(2.*1j*np.pi*(lm*uvw[0][0])*freq[0]/lightspeed)
 
-    print(np.stack([dft_grid.squeeze(), oned_grid_fft]))
-    print(dft_grid.squeeze().real - oned_grid_fft.real)
+    assert np.allclose(np_vis.real, dft_grid.squeeze().real)
 
+    print("vis", np.stack([np_vis, dft_grid.squeeze()], axis=1))
+
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        pass
+    else:
+        plt.figure()
+
+        arrays = [dft_grid.squeeze().real, oned_grid.real, taper]
+        names = ["dft", "fft", "taper"]
+
+        for name, array in zip(names, arrays):
+            plt.plot(array)
+
+        plt.legend(names)
+        plt.show()
