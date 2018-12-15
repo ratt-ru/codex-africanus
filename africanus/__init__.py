7cdd2b3c9cb801dccdb9c49698444dee5010cd8a
Simon Perkins <simon.perkins@gmail.com>
2018-12-15 16:28:21 +0200

    Remove logger

--- africanus/__init__.py
+++ africanus/__init__.py
@@ -6,28 +6,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import logging
-import logging.handlers
-
 __author__ = """Simon Perkins"""
 __email__ = 'sperkins@ska.ac.za'
 __version__ = '0.1.2'
-
-
-def _create_logger():
-    """ Setup logging configuration """
-
-    # Console formatter, mention name
-    cfmt = logging.Formatter(('%(name)s - %(levelname)s - %(message)s'))
-
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(cfmt)
-
-    # Create the logger, adding the console handler
-    log = logging.getLogger(__name__)
-    log.addHandler(ch)
-
-
-_create_logger()
