163e9fdce9e776acc9e1afcfba982792ee541a8b
Simon Perkins <simon.perkins@gmail.com>
2019-01-16 10:07:36 +0200

    Bake correctly type constants into phase delay
    numba/jax need constants to be explicit to avoid promotion to higher
    precision

--- africanus/rime/jax/phase.py
+++ africanus/rime/jax/phase.py
@@ -14,12 +14,20 @@ else:
     opt_import_error = None
 
 from africanus.constants import minus_two_pi_over_c
+import africanus.util.jax_init
 from africanus.util.requirements import requires_optional
 
 
 @requires_optional('jax', opt_import_error)
 def phase_delay(lm, uvw, frequency):
-    out_dtype = np.result_type(lm, uvw, frequency, np.complex64)
+    global minus_two_pi_over_c
+
+    const_type = np.result_type(lm, uvw, frequency)
+    out_dtype = np.result_type(const_type, np.complex64)
+
+    one = const_type.type(1.0)
+    complex_one = out_dtype.type(1j)
+    minus_two_pi_over_c = const_type.type(minus_two_pi_over_c)
 
     l = lm[:, 0, None, None]
     m = lm[:, 1, None, None]
@@ -28,10 +36,10 @@ def phase_delay(lm, uvw, frequency):
     v = uvw[None, :, 1, None]
     w = uvw[None, :, 2, None]
 
-    n = np.sqrt(1.0 - l**2 - m**2) - 1.0
+    n = np.sqrt(one - l**2 - m**2) - one
 
-    real_phase = l * u + m * v + n * w
-    real_phase = lm.dtype.type(minus_two_pi_over_c) * real_phase
+    real_phase = (l * u + m * v + n * w)
+    real_phase = minus_two_pi_over_c * real_phase
     real_phase *= frequency[None, None, :]
 
-    return np.exp(1j*real_phase)
+    return np.exp(complex_one*real_phase)
