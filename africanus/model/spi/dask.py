eba9b249dbd5d5cebe606489bae1ff3851fa51ad
Simon Perkins <simon.perkins@gmail.com>
2019-01-28 09:38:18 +0200

    Upgrade to dask 1.1.0 to use HighLevelGraphs (#71)
    * Update jaxlib == 0.1.6
    * numpy != 1.15.3

--- africanus/model/spi/dask.py
+++ africanus/model/spi/dask.py
@@ -6,17 +6,20 @@ from __future__ import print_function
 
 from functools import wraps
 
-from .component_spi import SPI_DOCSTRING
-from .component_spi import fit_spi_components as np_fit_spi_components
+from africanus.model.spi.component_spi import SPI_DOCSTRING
+from africanus.model.spi.component_spi import (
+                                fit_spi_components as np_fit_spi_components)
 
-from ...util.requirements import requires_optional
+from africanus.util.requirements import requires_optional
 
 import numpy as np
 
 try:
-    import dask.array as da
-except ImportError:
-    pass
+    from dask.array.core import blockwise
+except ImportError as e:
+    opt_import_error = e
+else:
+    opt_import_error = None
 
 
 @wraps(np_fit_spi_components)
@@ -34,24 +37,24 @@ def _fit_spi_components_wrapper(data, weights, freqs, freq0,
                                  dtype=dtype_)
 
 
-@requires_optional('dask.array')
+@requires_optional('dask.array', opt_import_error)
 def fit_spi_components(data, weights, freqs, freq0,
                        alphai=None, I0i=None,
                        tol=1e-6, maxiter=100,
                        dtype=np.float64):
     """ Dask wrapper fit_spi_components function """
-    return da.core.atop(_fit_spi_components_wrapper, ("vars", "comps"),
-                        data, ("comps", "chan"),
-                        weights, ("chan",),
-                        freqs, ("chan",),
-                        freq0, None,
-                        alphai, ("comps",) if alphai is not None else None,
-                        I0i, ("comps",) if I0i is not None else None,
-                        tol, None,
-                        maxiter, None,
-                        dtype, None,
-                        new_axes={"vars": 4},
-                        dtype=dtype)
+    return blockwise(_fit_spi_components_wrapper, ("vars", "comps"),
+                     data, ("comps", "chan"),
+                     weights, ("chan",),
+                     freqs, ("chan",),
+                     freq0, None,
+                     alphai, ("comps",) if alphai is not None else None,
+                     I0i, ("comps",) if I0i is not None else None,
+                     tol, None,
+                     maxiter, None,
+                     dtype, None,
+                     new_axes={"vars": 4},
+                     dtype=dtype)
 
 
 fit_spi_components.__doc__ = SPI_DOCSTRING.substitute(
