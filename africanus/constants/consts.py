b193c316a6a0bf0ae8682d9e269a33c902ca64b0
landmanbester <lbester@ska.ac.za>
2019-01-31 00:49:28 +0200

    Testing first version

--- africanus/constants/consts.py
+++ africanus/constants/consts.py
@@ -9,3 +9,6 @@ c = 2.99792458e8
 
 two_pi_over_c = 2 * 3.14 / c if on_rtd() else 2 * np.pi / c
 minus_two_pi_over_c = -two_pi_over_c
+
+DEG2RAD = 3.14 / 180.0 if on_rtd() else np.pi / 180.0
+ARCSEC2RAD = 3.14 / (180 * 3600) if on_rtd() else np.pi / (180 * 3600)
