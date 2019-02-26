dc13d97e8d73862bcbd1e114cfe78a1e1410ccec
Simon Perkins <simon.perkins@gmail.com>
2019-02-26 15:26:24 +0200

    time averaging factory

---
+++ africanus/averaging/row_iterator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import numba
+
+
+@numba.njit(nogil=True)
+def row_iterator(rows, bin_size):
+    row_bin = 0
+    bin_contents = 0
+
+    for row in range(rows - 1):
+        # We're adding something to the bin
+        # so we can reason about bin fullness upfront
+        bin_contents += 1
+        bin_full = bin_contents == bin_size
+
+        # Yield row, bin and whether the bin is full
+        yield row, row_bin, bin_full
+
+        # Only advance now
+        row += 1
+
+        # Reset bin variables if the bin is full
+        if bin_full:
+            row_bin += 1
+            bin_contents = 0
+
+    # Bin is always full on the last iteration
+    yield row, row_bin, True
--- africanus/averaging/tests/test_averaging_factory.py
+++ africanus/averaging/tests/test_averaging_factory.py
@@ -5,8 +5,55 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+import pytest
 
-from africanus.averaging.avg_factory import averaging_factory
+from africanus.averaging.time_avg_factory import time_averager_factory
+from africanus.averaging.row_iterator import row_iterator
+
+
+def test_row_iterator():
+    it = row_iterator(7, 3)
+    assert next(it) == (0, 0, False)
+    assert next(it) == (1, 0, False)
+    assert next(it) == (2, 0, True)
+    assert next(it) == (3, 1, False)
+    assert next(it) == (4, 1, False)
+    assert next(it) == (5, 1, True)
+    assert next(it) == (6, 2, True)
+
+    with pytest.raises(StopIteration):
+        next(it)
+
+    it = row_iterator(6, 3)
+    assert next(it) == (0, 0, False)
+    assert next(it) == (1, 0, False)
+    assert next(it) == (2, 0, True)
+    assert next(it) == (3, 1, False)
+    assert next(it) == (4, 1, False)
+    assert next(it) == (5, 1, True)
+
+    with pytest.raises(StopIteration):
+        next(it)
+
+    it = row_iterator(5, 3)
+    assert next(it) == (0, 0, False)
+    assert next(it) == (1, 0, False)
+    assert next(it) == (2, 0, True)
+    assert next(it) == (3, 1, False)
+    assert next(it) == (4, 1, True)
+
+    with pytest.raises(StopIteration):
+        next(it)
+
+    it = row_iterator(5, 2)
+    assert next(it) == (0, 0, False)
+    assert next(it) == (1, 0, True)
+    assert next(it) == (2, 1, False)
+    assert next(it) == (3, 1, True)
+    assert next(it) == (4, 2, True)
+
+    with pytest.raises(StopIteration):
+        next(it)
 
 
 def test_averaging_factory():
@@ -18,12 +65,10 @@ def test_averaging_factory():
     vis = (np.random.random((row, chan, corr)) +
            np.random.random((row, chan, corr))*1j)
 
-    time_avg = averaging_factory("TimeAverager", time.shape[0], 5, time.dtype)
-    vis_avg = averaging_factory("VisAverager", row, chan, corr,
-                                5, 5, vis.dtype)
+    TimeAverager = time_averager_factory(time.dtype)
+    time_avg = TimeAverager(time.shape[0], 5, time.dtype)
 
     for r in range(row):
         time_avg.add(time[r])
-        vis_avg.add(vis[r])
 
     print(time_avg.result)
