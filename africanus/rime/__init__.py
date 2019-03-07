b2eab06587790f0493a427a5338aba44c289676c
JoshVStaden <joshvstaden14@gmail.com>
2019-03-07 10:39:17 +0200

    Updated __init__.py to include shapelets, and got CPU shapelets working.

--- africanus/rime/__init__.py
+++ africanus/rime/__init__.py
@@ -6,3 +6,4 @@ from .beam_cubes import beam_cube_dde
 from .parangles import parallactic_angles
 from .zernike import zernike_dde
 from .predict import predict_vis
+from .shapelets import shapelet_dde
--- africanus/rime/shapelets.py
+++ africanus/rime/shapelets.py
@@ -1,36 +1,58 @@
 import numba
 import numpy as np
-import math
 import numpy.polynomial.hermite as hermite
+from numpy import sqrt, exp
+from scipy.special import hermite
 
+e = 2.7182818284590452353602874713527
+square_root_of_pi = 1.77245385091
+
+#@numba.jit(nogil=True, nopython=True, cache=True)
 def factorial(n):
-    if n == 0 or n == 1:
+    if n <= 1:
         return 1
-    ret_val = 1
-    for i in range(1, n):
-        ret_val *= i
-    return ret_val
+    ans = 1
+    for i in range(1, n + 1):
+        ans *= i
+    return ans
 
-
-@numba.jit(nogil=True, nopython=True, cache=True)
+#@numba.jit(nogil=True, nopython=True, cache=True)
 def basis_function(n, x, beta):
-    return (1 / sqrt(pow(2, n) * sqrt(np.pi) * factorial(n) * beta)) * hermite.hermval(x / beta, n) * exp(-(x ** 2) / (2 * beta * beta))
+    basis_component = ((2**n) * ((np.pi)**(0.5)) * factorial(n) * beta)**(-0.5)
+    exponential_component = hermite(n)(x / beta) * np.exp((-0.5) * (x**2) * (beta **(-2)))
+    return basis_component * exponential_component
 
-@numba.jit(nogil=True, nopython=True, cache=True)
-def shapelet_dde(coords, coeff_beta, shapelet_coeffs):
+#@numba.jit(nogil=True, nopython=True, cache=True)
+def shapelet_dde(coords, coeffs, beta):
+    """
+    shapelet_dde: computes the shapelet in Fourier space
+    Inputs:
+        coords: coordinates in (u,v) space with shape (2, nsrc, ntime, na, nchan)
+        coeffs: shapelet coefficients with shape (ant, chan, nmax1, nmax2)
+        beta: characteristic shapelet size with shape (ant, chan, 2)
+    Returns:
+        out_shapelets: Shapelet with shape (nsrc, ntime, na, nchan)
+    """
     _, nsrc, ntime, na, nchan = coords.shape
-    _, _, npoly = coeffs.shape
+    _, _, nmax1, nmax2 = coeffs.shape
     out_shapelets = np.empty((nsrc, ntime, na, nchan), dtype=np.complex128)
-    for src in range(nsrc):
-        for time in range(ntime):
-            for ant in range(na):
-                for chan in range(nchan):
-                    l, m = coords[:, src, time, ant, chan]
-                    coeff_subset = coeffs[ant, chan]
+    for ant in range(na):
+        for chan in range(nchan):
+            betax = beta[ant, chan, 0]
+            betay = beta[ant, chan, 1]
+            for src in range(nsrc):
+                for time in range(ntime):
+                    u = coords[0, src, time, ant, chan]
+                    v = coords[1, src, time, ant, chan]
                     shapelet_sum = 0
-                    beta = coeff_beta[ant, chan]
-                    for poly in range(npoly):
-                        beta, f = coeff_subset[poly]
-                        shapelet_sum += basis_function(poly, l, beta) * basis_function(poly, m, beta)
+                    for n1 in range(nmax1):
+                        for n2 in range(nmax2):
+                            #print("n1 is %d and n2 is %d" %(n1, n2))
+                            tmp_ans = coeffs[ant, chan, n1, n2] + 0j
+                            tmp_ans *= (1j**(n1)) * (1j ** (n2))
+                            tmp_ans *= basis_function(n1, u, betax**(-1))
+                            tmp_ans *= basis_function(n2, v, betay**(-1))
+                            shapelet_sum += tmp_ans
+                           # shapelet_sum += coeffs[ant, chan, n1, n2] * 1j**(n1 + n2) * basis_function(n1, u, 1/betax) * basis_function(n2, v, 1/betay)
                     out_shapelets[src, time, ant, chan] = shapelet_sum
     return out_shapelets
