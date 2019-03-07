b941f8263d386a9d55f478817e8a855e25889e55
Simon Perkins <simon.perkins@gmail.com>
2019-03-07 15:42:01 +0200

    Add fake numba jit and generated_jit decorators
    When on readthedocs, fake numba decorators to
    simply return the previous function.

--- africanus/util/numba.py
+++ africanus/util/numba.py
@@ -4,8 +4,32 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
+from decorator import decorate
 from numba import types
+
+from africanus.util.docs import on_rtd
+
+if on_rtd():
+    # Fake decorators when on readthedocs
+    def generated_jit(*args, **kwargs):
+        def decorator(fn):
+            def wrapper(*args, **kwargs):
+                return fn(*args, **kwargs)
+
+            return decorate(fn, wrapper)
+
+        return decorator
+
+    def jit(*args, **kwargs):
+        def decorator(fn):
+            def wrapper(*args, **kwargs):
+                return fn(*args, **kwargs)
+
+            return decorate(fn, wrapper)
+
+        return decorator
+else:
+    from numba import jit, generated_jit
 
 
 def is_numba_type_none(arg):
