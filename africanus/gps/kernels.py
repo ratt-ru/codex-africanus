58948fae7289de9fcc72ed179b3414cb2291cc3b
Landman Bester <lbester@ska.ac.za>
2019-06-05 10:42:35 +0200

    phase_only_gauss_newton calibration (#113)
    * and many calibration utilities

---
+++ africanus/gps/kernels.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+from africanus.gps.utils import abs_diff
+
+
+def exponential_squared(x, xp, sigmaf, l):
+    """
+    Create exponential squared covariance
+    function between :math:`D` dimensional
+    vectors :math:`x` and :math:`x_p` i.e.
+
+    .. math::
+        k(x, x_p) = \\sigma_f^2 \\exp\\left(-\\frac{(x-x_p)^2}{2l^2}\\right)
+
+    Parameters
+    ----------
+    x : :class:`numpy.ndarray`
+        Array of shape :code:`(N, D)`.
+    xp : :class:`numpy.ndarray`
+        Array of shape :code:`(Np, D)`.
+    sigmaf : float
+        The signal variance hyper-parameter
+    l : float
+        The length scale hyper-parameter
+
+    Returns
+    -------
+    K : :class:`numpy.ndarray`
+        Array of shape :code:`(N, Np)`
+    """
+    xxp = abs_diff(x, xp)
+    return sigmaf**2*np.exp(-xxp**2/(2*l**2))
