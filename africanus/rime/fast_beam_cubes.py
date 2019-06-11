d122799698e807ff6ad8339cf725ca8847fedfcf
Simon Perkins <simon.perkins@gmail.com>
2019-06-11 14:27:08 +0200

    Working comparison with montblanc

--- africanus/rime/fast_beam_cubes.py
+++ africanus/rime/fast_beam_cubes.py
@@ -13,17 +13,20 @@ import numpy as np
 from africanus.util.numba import njit
 
 
-@njit(nogil=True, cache=True)
+@njit(nogil=True)
 def freq_grid_interp(frequencies, beam_freq_map):
     # Interpolated grid coordinate
-    grid_pos_f = np.interp(frequencies, beam_freq_map,
-                           np.arange(beam_freq_map.size))
+    beam_nud = beam_freq_map.shape[0]
+    grids = np.arange(beam_freq_map.size).astype(np.float64)
+    grid_pos_f = np.interp(frequencies, beam_freq_map, grids)
     # Floor grid position
-    grid_pos = np.empty(grid_pos_f.shape, dtype=np.int32)
+    grid_pos = np.empty((grid_pos_f.shape[0], 2),
+                        dtype=np.int32)
     # Frequency scaling for cases when we're below or above the beam
     freq_scale = np.empty_like(frequencies)
     # Frequency difference between the channel frequency and the grid frequency
-    freq_grid_diff = np.empty(frequencies.shape, dtype=frequencies.dtype)
+    freq_grid_diff = np.empty((frequencies.shape[0], 2),
+                              dtype=frequencies.dtype)
 
     beam_nud = beam_freq_map.shape[0]
     beam_nud_f_minus_one = grid_pos_f.dtype.type(beam_freq_map.shape[0] - 1)
@@ -36,7 +39,7 @@ def freq_grid_interp(frequencies, beam_freq_map):
         # and introduce scaling for the lm coordinates
         if freq < beam_freq_map[0]:
             freq_scale[chan] = freq / beam_freq_map[0]
-            grid_pos[chan] = 0
+            grid_low = 0
             freq = beam_freq_map[0]
         # Above, or exactly on the last value of the beam_freq_map
         # clamp frequencies to the upper position and introduce
@@ -48,20 +51,31 @@ def freq_grid_interp(frequencies, beam_freq_map):
                 freq_scale[chan] = 1.0
 
             # Shift one grid point back from the end
-            grid_pos[chan] = beam_nud - 2
+            grid_low = beam_nud - 2
             freq = beam_freq_map[-1]
-        # Standard case
+        # Standard case, snap to the lower grid coordinate
         else:
-            grid_pos[chan] = np.floor(grid_pos_f[chan])
+            grid_low = int(np.floor(grid_pos_f[chan]))
             freq_scale[chan] = 1.0
 
-        # Difference between the frequency and the floor grid position freq
-        freq_grid_diff[chan] = freq - beam_freq_map[grid_pos[chan]]
+        grid_hi = min(grid_low + 1, beam_nud - 1)
+
+        grid_pos[chan] = grid_low, grid_hi
+
+        if grid_low == grid_hi:
+            assert grid_low == beam_nud - 1
+            freq_grid_diff[chan] = 1.0, 0.0
+        else:
+            lower_freq = beam_freq_map[grid_low]
+            upper_freq = beam_freq_map[grid_hi]
+            freq_diff = upper_freq - lower_freq
+            freq_grid_diff[chan, 0] = (upper_freq - freq) / freq_diff
+            freq_grid_diff[chan, 1] = (freq - lower_freq) / freq_diff
 
     return grid_pos, freq_scale, freq_grid_diff
 
 
-@njit(nogil=True, cache=True)
+# @njit(nogil=True, cache=True)
 def beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
                   lm, parangles, point_errors, antenna_scaling,
                   frequencies):
@@ -86,6 +100,8 @@ def beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
     # Maximum l and m indices in float and int
     lmaxf = ex_dtype.type(beam_lw - 1)
     mmaxf = ex_dtype.type(beam_mh - 1)
+    lmaxi = beam_lw - 1
+    mmaxi = beam_mh - 1
 
     lscale = lmaxf / (upper_l - lower_l)
     mscale = mmaxf / (upper_m - lower_m)
@@ -106,7 +122,6 @@ def beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
     corr_sum = np.zeros((ncorrs,), dtype=beam.dtype)
     absc_sum = np.zeros((ncorrs,), dtype=beam.real.dtype)
     beam_scratch = np.zeros((ncorrs,), dtype=beam.dtype)
-    weight = np.zeros((8,), dtype=ex_dtype)
 
     for t in range(ntime):
         for a in range(nants):
@@ -123,8 +138,8 @@ def beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
                     sm = m * freq_scale[f]
 
                     # Add pointing errors
-                    tl = sl + point_errors[t, a, 0]
-                    tm = sm + point_errors[t, a, 1]
+                    tl = sl + point_errors[t, a, f, 0]
+                    tm = sm + point_errors[t, a, f, 1]
 
                     # Rotate lm coordinate angle
                     vl = tl*cos_pa - tm*sin_pa
@@ -145,54 +160,85 @@ def beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
                     # Snap to the lower grid coordinates
                     gl0 = np.int32(np.floor(vl))
                     gm0 = np.int32(np.floor(vm))
-                    gc0 = grid_pos[f]
+                    gc0 = grid_pos[f, 0]
 
                     # Snap to the upper grid coordinates
-                    gc1 = gc0 + 1
+                    gl1 = min(gl0 + 1, lmaxi)
+                    gm1 = min(gm0 + 1, mmaxi)
+                    gc1 = grid_pos[f, 1]
 
                     # Difference between grid and offset coordinates
                     ld = vl - gl0
                     md = vm - gm0
-                    nud = freq_diff[f]
+                    nud, inv_nud = freq_diff[f]
 
                     # Zero accumulation arrays
                     corr_sum[:] = 0
                     absc_sum[:] = 0
 
-                    # Lower cube weights
-                    weight[0] = (one - ld)*(one - md)*nud
-                    weight[1] = ld*(one - md)*nud
-                    weight[2] = (one - ld)*md*nud
-                    weight[3] = ld*md*nud
-
-                    # Upper cube weights
-                    weight[4] = (one - ld)*(one - md)*(one - nud)
-                    weight[5] = ld*(one - md)*(one - nud)
-                    weight[6] = (one - ld)*md*(one - nud)
-                    weight[7] = ld*md*(one - nud)
-
                     # Accumulate lower cube correlations
-                    beam_scratch[:] = fbeam[gl0, gm0, gc0]
+                    beam_scratch[:] = fbeam[gl0, gm0, gc0, :]
+                    weight = (one - ld)*(one - md)*nud
 
                     for c in range(ncorrs):
-                        # Sensible compilers should unroll this loop
-                        for w in range(4):
-                            absc_sum[c] += weight[w] * np.abs(beam_scratch[c])
-                            corr_sum[c] += weight[w] * beam_scratch[c]
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    beam_scratch[:] = fbeam[gl1, gm0, gc0, :]
+                    weight = ld*(one - md)*nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    beam_scratch[:] = fbeam[gl0, gm1, gc0, :]
+                    weight = (one - ld)*md*nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    beam_scratch[:] = fbeam[gl1, gm1, gc0, :]
+                    weight = ld*md*nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
 
                     # Accumulate upper cube correlations
-                    beam_scratch[:] = fbeam[gl0, gm0, gc1]
+                    beam_scratch[:] = fbeam[gl0, gm0, gc1, :]
+                    weight = (one - ld)*(one - md)*inv_nud
 
                     for c in range(ncorrs):
-                        # Sensible compilers should unroll this loop
-                        for w in range(4, 8):
-                            absc_sum[c] += weight[w] * np.abs(beam_scratch[c])
-                            corr_sum[c] += weight[w] * beam_scratch[c]
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
 
+                    beam_scratch[:] = fbeam[gl1, gm0, gc1, :]
+                    weight = ld*(one - md)*inv_nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    beam_scratch[:] = fbeam[gl0, gm1, gc1, :]
+                    weight = (one - ld)*md*inv_nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    beam_scratch[:] = fbeam[gl1, gm1, gc1, :]
+                    weight = ld*md*inv_nud
+
+                    for c in range(ncorrs):
+                        absc_sum[c] += weight * np.abs(beam_scratch[c])
+                        corr_sum[c] += weight * beam_scratch[c]
+
+                    for c in range(ncorrs):
                         # Added all correlations, normalise
                         div = np.abs(corr_sum[c])
 
-                        if div != 0.0:
+                        if div == 0.0:
                             # This case probably works out to a zero assign
                             corr_sum[c] *= absc_sum[c]
                         else:
--- africanus/rime/tests/test_fast_beams.py
+++ africanus/rime/tests/test_fast_beams.py
@@ -20,6 +20,12 @@ def beam_freq_map():
 
 
 @pytest.fixture
+def beam_freq_map_montblanc():
+    """ Montblanc doesn't handle values outside the cube in the same way """
+    return np.array([.4, .56,  .7, .91, 1.1])
+
+
+@pytest.fixture
 def freqs():
     """
     Related to the beam_freq_map fixture.
@@ -34,7 +40,122 @@ def freqs():
     return np.array([.4, .5, .6, .7, .8, .9, 1.0, 1.1])
 
 
-def test_fast_beams(freqs, beam_freq_map):
+def test_fast_beam_small():
+    """ Small beam test, interpolation of one soure at [0.1, 0.1] """
+    np.random.seed(42)
+
+    # One frequency, to the lower side of the beam frequency map
+    freq = np.asarray([.3])
+    beam_freq_map = np.asarray([0.0, 1.0])
+
+    beam_lw = 2
+    beam_mh = 2
+    beam_nud = beam_freq_map.shape[0]
+
+    time = 1
+    ants = 1
+    chans = freq.shape[0]
+
+    beam_extents = np.asarray([[-1.0, 1.0], [-1.0, 1.0]])
+    lm = np.asarray([[0.1, 0.1]])
+
+    # Set all of the following to identity
+    parangles = np.zeros((time, ants))
+    point_errors = np.zeros((time, ants, chans, 2))
+    antenna_scaling = np.ones((ants, chans, 2))
+
+    beam = rc((beam_lw, beam_mh, beam_nud, 1))
+
+    ddes = beam_cube_dde(beam, beam_extents, beam_freq_map,
+                         lm, parangles, point_errors, antenna_scaling,
+                         freq)
+
+    # Pen and paper the expected value
+    lower_l = beam_extents[0, 0]
+    lower_m = beam_extents[1, 0]
+
+    ld = (lm[0, 0] - lower_l) / (beam_extents[0, 1] - lower_l)
+    md = (lm[0, 1] - lower_m) / (beam_extents[1, 1] - lower_m)
+    chd = freq[0]  # Hard-coded
+
+    corr_sum = np.zeros((1,), dtype=beam.dtype)
+    abs_sum = np.zeros((1,), dtype=beam.real.dtype)
+
+    # Weights of the sample at each grid point
+    wt0 = (1 - ld)*(1 - md)*(1 - chd)
+    wt1 = ld*(1 - md)*(1 - chd)
+    wt2 = (1 - ld)*md*(1 - chd)
+    wt3 = ld*md*(1 - chd)
+
+    wt4 = (1 - ld)*(1 - md)*chd
+    wt5 = ld*(1 - md)*chd
+    wt6 = (1 - ld)*md*chd
+    wt7 = ld*md*chd
+
+    # Sum lower channel correlations
+    corr_sum[:] += wt0 * beam[0, 0, 0, 0]
+    corr_sum[:] += wt1 * beam[1, 0, 0, 0]
+    corr_sum[:] += wt2 * beam[0, 1, 0, 0]
+    corr_sum[:] += wt3 * beam[1, 1, 0, 0]
+
+    abs_sum[:] += wt0 * np.abs(beam[0, 0, 0, 0])
+    abs_sum[:] += wt1 * np.abs(beam[1, 0, 0, 0])
+    abs_sum[:] += wt2 * np.abs(beam[0, 1, 0, 0])
+    abs_sum[:] += wt3 * np.abs(beam[1, 1, 0, 0])
+
+    # Sum upper channel correlations
+    corr_sum[:] += wt4 * beam[0, 0, 1, 0]
+    corr_sum[:] += wt5 * beam[1, 0, 1, 0]
+    corr_sum[:] += wt6 * beam[0, 1, 1, 0]
+    corr_sum[:] += wt7 * beam[1, 1, 1, 0]
+
+    abs_sum[:] += wt4 * np.abs(beam[0, 0, 1, 0])
+    abs_sum[:] += wt5 * np.abs(beam[1, 0, 1, 0])
+    abs_sum[:] += wt6 * np.abs(beam[0, 1, 1, 0])
+    abs_sum[:] += wt7 * np.abs(beam[1, 1, 1, 0])
+
+    corr_sum *= abs_sum / np.abs(corr_sum)
+
+    assert_array_almost_equal([[[[[0.470255+0.4786j]]]]], ddes)
+    assert_array_almost_equal(ddes.squeeze(), corr_sum.squeeze())
+
+
+def test_grid_interpolate(freqs, beam_freq_map):
+    grid_pos, freq_scale, fgrid_diff = freq_grid_interp(freqs, beam_freq_map)
+
+    # Frequencies (first -- 0.8 and last -- 1.1)
+    # outside the beam result in scaling,
+    assert_array_almost_equal(freq_scale,
+                              [0.8, 1.,  1.,  1.,  1.,  1.,  1.,  1.1])
+    # Frequencies outside the beam are snapped to 0 if below
+    # and beam_nud - 2 if above.
+    # Frequencies on the edges are similarly snapped
+    assert_array_equal(grid_pos[:, 0], [0, 0, 1, 2, 2, 2, 3, 3])
+    # Frequency less grid position frequency.
+    # frequency is snapped to the first and last beam freq value
+    # if outside (first and last values)
+    # Third frequency value is also exactly on a grid point
+    exp_diff = [[1., 0.],
+                [1., 0.],
+                [0.71428571, 0.28571429],
+                [1., 0.],
+                [0.52380952, 0.47619048],
+                [0.04761905, 0.95238095],
+                [0., 1.],
+                [0., 1.]]
+
+    assert_array_almost_equal(fgrid_diff, exp_diff)
+
+
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
+def test_fast_beams_vs_montblanc(freqs, beam_freq_map_montblanc, dtype):
+    """ Test that the numba beam matches montblanc implementation """
+    _ = pytest.importorskip("montblanc")
+    tf = pytest.importorskip("tensorflow")
+
+    freqs = freqs.astype(dtype)
+    beam_freq_map = beam_freq_map_montblanc.astype(dtype)
+
     beam_lw = 10
     beam_mh = 10
     beam_nud = beam_freq_map.shape[0]
@@ -44,32 +165,62 @@ def test_fast_beams(freqs, beam_freq_map):
     ants = 4
     chans = freqs.shape[0]
 
+    ctype = np.result_type(dtype, np.complex64)
+
     # Source Transform variables
-    lm = np.random.random(size=(src, 2))
-    parangles = np.random.random(size=(time, ants))
-    point_errors = np.random.random(size=(time, ants, 2))
-    antenna_scaling = np.random.random(size=(ants, chans, 2))
+    lm = np.random.random(size=(src, 2)).astype(dtype)
+    parangles = np.random.random(size=(time, ants)).astype(dtype)
+    point_errors = np.random.random(size=(time, ants, chans, 2)).astype(dtype)
+    antenna_scaling = np.random.random(size=(ants, chans, 2)).astype(dtype)
+
+    # Make random values more representative
+    lm = (lm - 0.5)*0.0001        # Shift lm to around the centre
+    parangles *= np.pi / 6        # parangles to 30 degrees
+    point_errors *= 0.001         # Pointing errors
+    antenna_scaling *= 0.0001     # Antenna scaling
 
     # Beam variables
-    beam = rc((beam_lw, beam_mh, beam_nud, 2, 2))
-    beam_lm_extents = np.asarray([[-1.0, 1.0], [-1.0, 1.0]])
+    beam = rc((beam_lw, beam_mh, beam_nud, 2, 2)).astype(ctype)
+    beam_lm_extents = np.asarray([[-1.0, 1.0], [-1.0, 1.0]]).astype(dtype)
 
-    grid_pos, freq_scale, fgrid_diff = freq_grid_interp(freqs, beam_freq_map)
-
-    # Frequencies (first -- 0.8 and last -- 1.1)
-    # outside the beam result in scaling,
-    assert_array_equal(freq_scale, [0.8, 1.,  1.,  1.,  1.,  1.,  1.,  1.1])
-    # Frequencies outside the beam are snapped to 0 if below
-    # and beam_nud - 2 if above.
-    # Frequencies on the edges are similarly snapped
-    assert_array_equal(grid_pos, [0, 0, 1, 2, 2, 2, 3, 3])
-    # Frequency less grid position frequency.
-    # frequency is snapped to the first and last beam freq value
-    # if outside (first and last values)
-    # Third frequency value is also exactly on a grid point
-    exp_diff = [0., 0., 0.04, 0., 0.1, 0.2, 0.09, 0.09]
-    assert_array_almost_equal(fgrid_diff, exp_diff)
-
-    ddes = beam_cube_dde(beam, beam_lm_extents, beam_freq_map,  # noqa
+    ddes = beam_cube_dde(beam, beam_lm_extents, beam_freq_map,
                          lm, parangles, point_errors, antenna_scaling,
                          freqs)
+
+    assert ddes.shape == (src, time, ants, chans, 2, 2)
+
+    from montblanc.impl.rime.tensorflow import load_tf_lib
+    rime = load_tf_lib()
+
+    # Montblanc beam extent format is different
+    mb_beam_extents = np.array([beam_lm_extents[0, 0],
+                                beam_lm_extents[1, 0],
+                                beam_freq_map[0],
+                                beam_lm_extents[0, 1],
+                                beam_lm_extents[1, 1],
+                                beam_freq_map[-1]], dtype=dtype)
+
+    # Montblanc wants flattened correlations
+    mb_beam = beam.reshape(beam.shape[:3] + (-1,))
+
+    np_args = [lm, freqs, point_errors, antenna_scaling,
+               np.sin(parangles), np.cos(parangles),
+               mb_beam_extents, beam_freq_map, mb_beam]
+    # Argument string name list
+    arg_names = ["lm", "frequency", "point_errors", "antenna_scaling",
+                 "parallactic_angle_sin", "parallactic_angle_cos",
+                 "beam_extents", "beam_freq_map", "e_beam"]
+
+    # Constructor tensorflow variables
+    tf_args = [tf.Variable(v, name=n) for v, n in zip(np_args, arg_names)]
+
+    with tf.device("/cpu:0"):
+        tf_dde = rime.e_beam(*tf_args)
+
+    init_op = tf.global_variables_initializer()
+
+    with tf.Session() as S:
+        S.run(init_op)
+        res = S.run(tf_dde)
+
+    assert_array_almost_equal(res, ddes.reshape(ddes.shape[:4] + (-1,)))
