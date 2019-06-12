efc554256bb49ec9b6890922bce27485e728d702
Simon Perkins <simon.perkins@gmail.com>
2019-06-12 12:26:07 +0200

    Greatly expand test parameters

--- africanus/gridding/facet/tests/test_spheroidal.py
+++ africanus/gridding/facet/tests/test_spheroidal.py
@@ -5,24 +5,44 @@ from __future__ import division
 from __future__ import print_function
 
 
+import numpy as np
 from numpy.testing import assert_array_almost_equal
 import pytest
 
-from africanus.gridding.facet.spheroidal import (np_spheroidal_2d,
+from africanus.gridding.facet.spheroidal import (delta_n_coefficients,
+                                                 np_spheroidal_2d,
                                                  spheroidal_2d,
-                                                 spheroidal_aa_filter as spaaf)
+                                                 spheroidal_aa_filter as spaaf,
+                                                 wplanes)
 
+@pytest.fixture
+def freqs():
+    return np.linspace(.856e9, 2*.856e9, 4)
 
-
-@pytest.mark.parametrize("support", [10, 11, 12])
-@pytest.mark.parametrize("spheroidal_support", [110, 111, 112])
+# @pytest.mark.parametrize("support", [10, 11, 12])
+# @pytest.mark.parametrize("spheroidal_support", [110, 111, 112])
+# @pytest.mark.parametrize("npix", [1025])
+# @pytest.mark.parametrize("wlayers", [11, 7])
+@pytest.mark.parametrize("support", [11])
+@pytest.mark.parametrize("spheroidal_support", [111])
 @pytest.mark.parametrize("npix", [1025])
-def test_spheroidal_vs_ddfacet(support, spheroidal_support, npix, tmpdir):
+@pytest.mark.parametrize("wlayers", [7])
+@pytest.mark.parametrize("maxw", [30000])
+@pytest.mark.parametrize("cell_size", [1.3])
+@pytest.mark.parametrize("oversampling", [11])
+@pytest.mark.parametrize("lm_shift", [(1e-8, 1e-8)])
+def test_spheroidal_vs_ddfacet(support, spheroidal_support,
+                               npix, wlayers, maxw,
+                               cell_size, oversampling,
+                               lm_shift,
+                               freqs, tmpdir):
     pytest.importorskip("DDFacet")
 
-    from DDFacet.Imager.ModCF import SpheMachine
+    from DDFacet.Imager.ModCF import SpheMachine, ClassWTermModified, Give_dn
+    from DDFacet.Array.shared_dict import SharedDict
     from DDFacet.ToolsDir import ModTaper
 
+    # Test the spheroidal's match
     numba_spheroidal = spheroidal_2d(support)
     numpy_spheroidal = np_spheroidal_2d(support)
     ddf_spheroidal = ModTaper.Sphe2D(support)
@@ -33,6 +53,7 @@ def test_spheroidal_vs_ddfacet(support, spheroidal_support, npix, tmpdir):
     # Africanus and DDFacet implementations match
     assert_array_almost_equal(numba_spheroidal, ddf_spheroidal)
 
+    # Test that SpheMachine and spheroidal_aa_filter do the same thing
     cf, fcf, ifzfcf = spaaf(npix, support=support,
                             spheroidal_support=spheroidal_support)
 
@@ -43,11 +64,21 @@ def test_spheroidal_vs_ddfacet(support, spheroidal_support, npix, tmpdir):
     assert_array_almost_equal(fcf, ddf_fcf)
     assert_array_almost_equal(ifzfcf, ddf_ifzfcf)
 
-    from DDFacet.Imager.ModCF import ClassWTermModified
-    from DDFacet.Array.shared_dict import SharedDict
+    radius_deg = (npix / 2.0) * cell_size / 3600.
+    radius_lm = radius_deg * np.pi / 180.
 
-    cf_dict = SharedDict(path=str(tmpdir))
-    wterm = ClassWTermModified(cf_dict=cf_dict, Sup=support, Npix=npix)
+    # Check that we produce the same delta n
+    ddf_cl, ddf_cm, ddf_dn = Give_dn(*lm_shift, rad=radius_lm)
+    afr_cl, afr_cm, afr_dn = delta_n_coefficients(*lm_shift, radius=radius_lm)
+    assert_array_almost_equal(ddf_cl, afr_cl)
+    assert_array_almost_equal(ddf_cm, afr_cm)
+    assert_array_almost_equal(ddf_dn, afr_dn)
+
+    cf_dict = SharedDict(str(tmpdir))
+
+    wterm = ClassWTermModified(cf_dict=cf_dict, Sup=support, Npix=npix,
+                               Freqs=freqs, Nw=wlayers, wmax=maxw,
+                               OverS=oversampling, lmShift=lm_shift)
 
     ddf_cf, ddf_fcf, ddf_ifzfcf = wterm.SpheM.MakeSphe(npix)
     cf, fcf, ifzfcf = spaaf(npix, support=support)
@@ -56,3 +87,6 @@ def test_spheroidal_vs_ddfacet(support, spheroidal_support, npix, tmpdir):
     assert_array_almost_equal(fcf, ddf_fcf)
     assert_array_almost_equal(ifzfcf, ddf_ifzfcf)
 
+    wplanes(wlayers, cell_size, support, maxw,
+            npix, oversampling,
+            lm_shift, freqs)
