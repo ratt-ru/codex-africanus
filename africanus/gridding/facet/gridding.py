478c525c261cb553b7d4799db19781d44d03eeb6
Simon Perkins <simon.perkins@gmail.com>
2019-06-13 18:16:35 +0200

    Create gridding outline

--- africanus/gridding/facet/gridding.py
+++ africanus/gridding/facet/gridding.py
@@ -34,7 +34,155 @@ Smear = namedtuple("Smear", [
     "freq_smear"])
 
 
-def output_factory(have_vis):
+_ARCSEC2RAD = np.deg2rad(1.0/(60*60))
+
+
+def grid_out_factory(have_sizes, have_grid):
+    if have_sizes and have_grid:
+        def impl(ny, nx, ncorr, grid, dtype):
+            if grid.shape != (ny, nx, ncorr):
+                raise ValueError("grid.shape != (ny, nx, ncorr")
+
+            return grid
+    elif have_sizes and not have_grid:
+        def impl(ny, nx, ncorr, grid, dtype):
+            return np.zeros((ny, nx, ncorr), dtype=dtype)
+    elif not have_sizes and have_grid:
+        def impl(ny, nx, ncorr, grid, dtype):
+            return grid
+    elif not have_sizes and not have_grid:
+        raise ValueError("Either 'nx AND ny' OR 'grid' must be supplied")
+
+    return njit(nogil=True, cache=True)(impl)
+
+
+@generated_jit(nopython=True, nogil=True, cache=True)
+def grid(vis, uvw, flags, weights, freqs,
+         w_planes, w_planes_conj, meta,
+         grid=None, ny=None, nx=None):
+
+    have_ny = not is_numba_type_none(ny)
+    have_nx = not is_numba_type_none(nx)
+    have_grid = not is_numba_type_none(grid)
+
+    create_grid = grid_out_factory(have_nx and have_ny, have_grid)
+    out_dtype = vis.dtype
+
+    def impl(vis, uvw, flags, weights, freqs,
+             w_planes, w_planes_conj, meta,
+             grid=None, ny=None, nx=None):
+        nrow, nchan, ncorr = vis.shape
+        grid = create_grid(ny, nx, ncorr, grid, out_dtype)
+        ny, nx = grid.shape[:2]
+
+        maxw = meta.maxw
+        ref_wave = meta.ref_wave
+        overs = meta.oversampling
+        nw = len(w_planes)
+
+        if nw != len(w_planes_conj):
+            raise ValueError("Number of kernels and conjugates differ")
+
+        for cf, cf_conj in zip(w_planes, w_planes_conj):
+            if cf.shape[0] % 2 != 1 or cf.shape[1] % 2 != 1:
+                raise ValueError("w-projection kernels must be odd")
+
+            if cf_conj.shape[0] % 2 != 1 or cf_conj.shape[1] % 2 != 1:
+                raise ValueError("conjugate w-projection kernels must be odd")
+
+        if ny % 2 != 1 or nx % 2 != 1:
+            raise ValueError("Grid must be odd")
+
+        half_y = np.float64(ny // 2)
+        half_x = np.float64(nx // 2)
+
+        # # UV scaling in radians
+        u_scale = np.float64(_ARCSEC2RAD * meta.cell_size_x * nx)
+        v_scale = np.float64(_ARCSEC2RAD * meta.cell_size_y * ny)
+
+        for r in range(nrow):
+            # Extract UVW coordinate, incorporating 1st order polynomial
+            w = uvw[r, 2]
+            u = uvw[r, 0] + w*meta.cu
+            v = uvw[r, 1] + w*meta.cv
+
+            for f in range(nchan):
+                # Exit early if all correlations flagged
+                if np.all(flags[r, f, :]):
+                    continue
+
+                inv_wave = freqs[f] / lightspeed
+                # Look up the w projection kernel
+                wi = int(np.round((nw - 1)*np.abs(w)*ref_wave*inv_wave/maxw))
+
+                if wi >= nw:
+                    continue
+
+                w_kernel = w_planes[wi] if w > 0 else w_planes_conj[wi]
+                ncy, ncx = w_kernel.shape
+
+                # This calculation probably depends on an odd support size
+                support_y = ncy // overs
+                support_x = ncx // overs
+                half_sup_y = support_y // 2
+                half_sup_x = support_x // 2
+
+                # Scale UV coordinate into the [0:ny, 0:nx]
+                # coordinate system
+                pos_x = u * u_scale * inv_wave + half_x
+                pos_y = v * v_scale * inv_wave + half_y
+
+                # Snap to grid point (float and integer)
+                loc_yf = np.round(pos_y)
+                loc_xf = np.round(pos_x)
+                loc_yi = int(loc_yf)
+                loc_xi = int(loc_xf)
+
+                # Only degrid vis if the full support lies within the grid
+                if (loc_xi - half_sup_x < 0 or
+                    loc_xi + half_sup_x >= nx or
+                    loc_yi - half_sup_y < 0 or
+                        loc_yi + half_sup_y >= ny):
+                    continue
+
+                # Oversampling index in a [-n/2, n/2] coordinate system
+                overs_y = int(np.round((loc_yf - pos_y)*overs))
+                overs_x = int(np.round((loc_xf - pos_x)*overs))
+
+                # Shift to a [0, n] coordinate system
+                overs_y = (overs_y + ((3 * overs) // 2)) % overs
+                overs_x = (overs_x + ((3 * overs) // 2)) % overs
+
+                # Dereference the appropriate kernel
+                # associated with these oversampling factors
+                w_kernel_exp = w_kernel.reshape((overs, overs,
+                                                 support_y, support_x))
+
+                sub_kernel = w_kernel_exp[overs_y, overs_x, :, :]
+
+                # Iterate over the convolution kernel
+                # accumulating values from the grid
+                for sy in range(0, support_y):
+                    gy = loc_yi + sy  # Grid position in y
+
+                    for sx in range(0, support_x):
+                        gx = loc_xi + sx                # Grid position in x
+                        weight = sub_kernel[sy, sx]     # Filter value
+
+                        for c in range(ncorr):
+                            # Ignore if flagged
+                            if flags[r, f, c] != 0:
+                                continue
+
+                            # Accumulate visibility onto the grid
+                            grid[gy, gx, c] += vis[r, f, c] * weight
+
+        return grid
+
+    return impl
+
+
+def vis_out_factory(have_vis):
     if have_vis:
         def impl(nrow, nchan, ncorr, vis, dtype):
             if vis.shape != (nrow, nchan, ncorr):
@@ -53,9 +201,7 @@ def degrid(grid, uvw, freqs,
            w_planes, w_planes_conj, meta,
            vis=None):
 
-    _ARCSEC2RAD = np.deg2rad(1.0/(60*60))
-
-    create_output = output_factory(not is_numba_type_none(vis))
+    create_output = vis_out_factory(not is_numba_type_none(vis))
 
     def impl(grid, uvw, freqs,
              w_planes, w_planes_conj, meta,
@@ -108,7 +254,6 @@ def degrid(grid, uvw, freqs,
                     continue
 
                 w_kernel = w_planes[wi] if w > 0 else w_planes_conj[wi]
-
                 ncy, ncx = w_kernel.shape
 
                 # This calculation probably depends on an odd support size
--- africanus/gridding/facet/tests/test_gridder.py
+++ africanus/gridding/facet/tests/test_gridder.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 from africanus.constants import c as lightspeed
-from africanus.gridding.facet.gridding import degrid, Metadata
+from africanus.gridding.facet.gridding import grid, degrid, Metadata
 from africanus.gridding.facet.spheroidal import wplanes
 
 import numpy as np
@@ -54,3 +54,42 @@ def test_degridder(support, spheroidal_support, npix,
 
     vis = degrid(grid, uvw, freqs, wcf, wcf_conj, meta)
     assert vis.shape == (uvw.shape[0], freqs.shape[0], grid.shape[2])
+
+
+@pytest.mark.parametrize("support", [11])
+@pytest.mark.parametrize("spheroidal_support", [111])
+@pytest.mark.parametrize("npix", [1025])
+@pytest.mark.parametrize("wlayers", [7])
+@pytest.mark.parametrize("maxw", [30000])
+@pytest.mark.parametrize("cell_size", [1.3])
+@pytest.mark.parametrize("oversampling", [11])
+@pytest.mark.parametrize("lm_shift", [(1e-8, 1e-8)])
+def test_gridder(support, spheroidal_support, npix,
+                 wlayers, maxw, cell_size,
+                 oversampling, lm_shift):
+
+    np.random.seed(42)
+    nrow = 10
+    nchan = 8
+    ncorr = 4
+
+    uvw = rf((nrow, 3)) - 0.5
+    uvw[:, :2] *= 1e4
+    vis = rc((nrow, nchan, ncorr))
+    flags = np.random.randint(0, 2, (nrow, nchan, ncorr))
+    weights = np.random.random((nrow, nchan, ncorr))
+    freqs = np.linspace(.856e9, 2*.856e9, nchan)
+
+    ref_wave = freqs[nchan // 2] / lightspeed
+
+    cu, cv, wcf, wcf_conj = wplanes(wlayers, cell_size, support, maxw,
+                                    npix, oversampling,
+                                    lm_shift, freqs)
+
+    meta = Metadata(lm_shift[0], lm_shift[1], ref_wave, maxw,
+                    oversampling, cell_size, cell_size,
+                    cu, cv)
+
+    grid_ = grid(vis, uvw, flags, weights, freqs, wcf, wcf_conj, meta,
+                 ny=npix, nx=npix)
+
