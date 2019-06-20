5dd8139d81787a2ae5079c72daaf377be8853156
JoshVStaden <g14v2805@campus.ru.ac.za>
2019-06-20 16:04:04 +0200

    Got the 2D shapelets working, and added the w term in.

--- africanus/model/shape/__init__.py
+++ africanus/model/shape/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["gaussian", "shapelet"]
 
 from africanus.model.shape.gaussian_shape import gaussian
-from africanus.model.shape.shapelets import shapelet
+from africanus.model.shape.shapelets import shapelet, basis_function, shapelet_1d, shapelet_2d
