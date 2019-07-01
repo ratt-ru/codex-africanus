f865a7b4cab5fac67fd2e148ea0ab7d40af5908a
Simon Perkins <simon.perkins@gmail.com>
2019-07-01 11:00:34 +0200

    Mark parallactic angle tests as flaky

--- africanus/__init__.py
+++ africanus/__init__.py
@@ -6,6 +6,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# NOTE(sjperkins)
+# Imports at this level within this module should be avoided,
+# or should fail gracefully as this is the base africanus module.
+# The setup.py file accesses the ``africanus.install`` modules
 import africanus.util.jax_init  # noqa
 
 __author__ = """Simon Perkins"""
