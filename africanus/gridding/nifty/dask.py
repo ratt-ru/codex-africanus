21d5364b5425ae82ec3460897db679c4fc142aac
Simon Perkins <simon.perkins@gmail.com>
2019-07-09 18:11:01 +0200

    Try catch the ImportError

--- africanus/gridding/nifty/dask.py
+++ africanus/gridding/nifty/dask.py
@@ -45,6 +45,7 @@ class GridderConfigWrapper(object):
                 (self.nx, self.ny, self.eps, self.csx, self.csy))
 
 
+@requires_optional("dask.array", "nifty_gridder", import_error)
 @normalize_token.register(GridderConfigWrapper)
 def normalize_gridder_config_wrapper(gc):
     return normalize_token((gc.nx, gc.ny, gc.csx, gc.csy, gc.eps))
