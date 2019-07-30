44f68ec80a612f0ca9833b6bf9065a19a1f0d917
Sakhile Masoka <sakhile.harvey@gmail.com>
2019-07-30 10:51:26 +0200

    test baseline time and channel, not complete

--- africanus/averaging/tests/test_baseline_time_and_channel.py
+++ africanus/averaging/tests/test_baseline_time_and_channel.py
@@ -12,6 +12,7 @@ import sys
 sys.path.insert(0, '/Users/smasoka/Varsity/codex-africanus/africanus/averaging/')
 from support import unique_time, unique_baselines
 from baseline_time_and_channel_mapping import baseline_row_mapper
+from baseline_time_and_channel_avg import baseline_time_and_channel
 
 
 nchan = 16
@@ -42,3 +43,24 @@ vis = (np.arange(row*chan*fcorrs, dtype=np.float32) +
       np.arange(1, row*chan*fcorrs+1, dtype=np.float32)*1j)
 vis = flat_vis.reshape(row, chan, fcorrs)
 flag = np.random.randint(0, 2, (row, chan, fcorrs))
+flag_row = np.zeros(time.shape, dtype=np.uint8)
+flagged_rows = None #, [0, 1], [2, 4], range(10)])
+flag_row[flagged_rows] = 1
+
+avg = baseline_time_and_channel(time, interval, ant1, ant2,
+                     time_centroid=time, exposure=interval, flag_row,
+                     uvw, weight, sigma,vis, flag,weight_spectrum, sigma_spectrum,
+                     bins_for_longest_baseline=1.0)
+
+print("avg.time \n", avg.time)
+print("avg.interval \n", avg.interval)
+print("avg.antenna1 \n", avg.antenna1)
+print("avg.antenna2", avg.antenna2)
+print("avg.time_centroid", avg)
+print("avg.exposure \n", avg.exposure)
+print("avg.uvw \n", avg.uvw)
+print("avg.weight \n", avg.weight)
+print("avg.sigma \n", avg.sigma)
+
+# I need channel averaging
+
