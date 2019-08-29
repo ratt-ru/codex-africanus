b70d03b118e473bdefea3ff07f880cc81d827f91
landmanbester <lbester@ska.ac.za>
2019-08-29 16:46:34 +0200

    Added compute_and_corrup_vis fn

--- africanus/calibration/utils/dask.py
+++ africanus/calibration/utils/dask.py
@@ -10,6 +10,7 @@ from africanus.calibration.utils.correct_vis import CORRECT_VIS_DOCS
 from africanus.calibration.utils.corrupt_vis import CORRUPT_VIS_DOCS
 from africanus.calibration.utils.residual_vis import RESIDUAL_VIS_DOCS
 from africanus.calibration.utils import correct_vis as np_correct_vis
+from africanus.calibration.utils import compute_and_corrupt_vis as np_compute_and_corrupt_vis
 from africanus.calibration.utils import corrupt_vis as np_corrupt_vis
 from africanus.calibration.utils import residual_vis as np_residual_vis
 from africanus.calibration.utils import check_type
@@ -68,6 +69,52 @@ def corrupt_vis(time_bin_indices, time_bin_counts, antenna1,
                      dtype=model.dtype,
                      align_arrays=False)
 
+
+@wraps(np_compute_and_corrupt_vis)
+def _compute_and_corrupt_vis_wrapper(time_bin_indices, time_bin_counts,
+                                     antenna1, antenna2, jones, model,
+                                     uvw, freq, lm):
+    return np_compute_and_corrupt_vis(time_bin_indices, time_bin_counts,
+                                      antenna1, antenna2, jones[0][0],
+                                      model[0], uvw[0], freq, lm[0][0])
+
+
+@requires_optional('dask.array', dask_import_error)
+def compute_and_corrupt_vis(time_bin_indices, time_bin_counts,
+                            antenna1, antenna2, jones, model,
+                            uvw, freq, lm):
+
+    mode = check_type(jones, model, vis_type='model')
+
+    if mode == DIAG_DIAG:
+        out_shape = ("row", "chan", "corr1")
+        model_shape = ("row", "chan", "dir", "corr1")
+        jones_shape = ("row", "ant", "chan", "dir", "corr1")
+    elif mode == DIAG:
+        out_shape = ("row", "chan", "corr1", "corr2")
+        model_shape = ("row", "chan", "dir", "corr1", "corr2")
+        jones_shape = ("row", "ant", "chan", "dir", "corr1")
+    elif mode == FULL:
+        out_shape = ("row", "chan", "corr1", "corr2")
+        model_shape = ("row", "chan", "dir", "corr1", "corr2")
+        jones_shape = ("row", "ant", "chan", "dir", "corr1", "corr2")
+    else:
+        raise ValueError("Unknown mode argument of %s" % mode)
+
+    return blockwise(_corrupt_vis_wrapper, out_shape,
+                     time_bin_indices, ("row",),
+                     time_bin_counts, ("row",),
+                     antenna1, ("row",),
+                     antenna2, ("row",),
+                     jones, jones_shape,
+                     model, model_shape,
+                     uvw, ("row", "three")
+                     freq, ("chan",),
+                     lm, ("row", "dir", "two")
+                     adjust_chunks={"row": antenna1.chunks[0]},
+                     new_axes={"corr2": 2},  # why?
+                     dtype=model.dtype,
+                     align_arrays=False)
 
 @wraps(np_correct_vis)
 def _correct_vis_wrapper(time_bin_indices, time_bin_counts, antenna1,
---
+++ africanus/calibration/utils/examples/apply_gains_time_varying_sources.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This example show how to apply gains to an ms in a chunked up way.
+The gains should be stored in a .npy file and have the shape
+expected by the corrupt_vis function.
+It is assumed that the direction axis is ordered in the same way as
+model_cols where model_cols is a comma separated string
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+from africanus.calibration.utils.dask import compute_and_corrupt_vis
+from africanus.calibration.utils import chunkify_rows
+from africanus.dft.dask import im_to_vis
+import xarray as xr
+from xarrayms import xds_from_ms, xds_to_table
+from pyrap.tables import table
+import dask.array as da
+from dask.diagnostics import ProgressBar
+
+import argparse
+
+def create_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument("--ms", help="Name of measurement set", type=str)
+    p.add_argument("--model_file", help=".npy file containing the 
+                   "time variable model in format [time, flux, chan, corr].",
+                   type=str)
+    p.add_argument("--coord_file", type=str, help="file containing source "
+                   "coordinates in format [time, l, m].")
+    p.add_argument("--data_col", help="Column where data lives. "
+                   "Only used to get shape of data at this stage",
+                   default='DATA', type=str)
+    p.add_argument("--out_col", help="Where to write the corrupted data to. "
+                   "Must exist in MS before writing to it.",
+                   default='CORRECTED_DATA', type=str)
+    p.add_argument("--gain_file", help=".npy file containing gains in format "
+                   "(time, antenna, freq, source, corr). "
+                   "See corrupt_vis docs.", type=str)
+    p.add_argument("--utimes_per_chunk",  default=1, type=int,
+                   help="Number of unique times in each chunk.")
+    p.add_argument("--ncpu", help="The number of threads to use. "
+                   "Default of zero means all", default=10, type=int)
+    p.add_argument('--field', default=0, type=int)
+    return p
+
+
+args = create_parser().parse_args()
+
+if args.ncpu:
+    ncpu = args.ncpu
+    from multiprocessing.pool import ThreadPool
+    import dask
+    dask.config.set(pool=ThreadPool(ncpu))
+else:
+    import multiprocessing
+    ncpu = multiprocessing.cpu_count()
+
+print("Using %i threads" % ncpu)
+
+# get full time column and compute row chunks
+time = table(args.ms).getcol('TIME')
+row_chunks, tbin_idx, tbin_counts = chunkify_rows(time, args.utimes_per_chunk)
+# convert to dask arrays
+tbin_idx = da.from_array(tbin_idx, chunks=(args.utimes_per_chunk))
+tbin_counts = da.from_array(tbin_counts, chunks=(args.utimes_per_chunk))
+n_time = tbin_idx.size
+
+# get freqs
+freqs = table(args.ms+'::SPECTRAL_WINDOW').getcol('CHAN_FREQ')
+n_freq = freqs.size
+freqs = da.from_array(freqs, chunks=(n_freq))
+
+# get source coordinates
+lm = np.load(args.coord_file)
+assert lm.shape[0] == n_time
+
+# load in the model file
+model = np.load(args.model_file)
+
+assert model.shape[0] == n_time
+assert model.shape[2] == n_freq
+
+# get number of sources
+n_dir = model.shape[1]
+assert lm.shape[1] == n_dir
+
+lm = da.from_array(lm, chunks=(args.utimes_per_chunk, n_dir, 2))
+
+# append antenna columns
+cols = []
+cols.append('ANTENNA1')
+cols.append('ANTENNA2')
+cols.append('UVW')
+cols.append(args.data_col)
+
+# load in gains
+jones = np.load(args.gain_file)
+jones = jones.astype(np.complex64)
+jones_shape = jones.shape
+ndims = len(jones_shape)
+jones = da.from_array(jones, chunks=(args.utimes_per_chunk,)
+                      + jones_shape[1:])
+
+# change model to dask array
+model = da.from_array(model, chunks=(args.utimes_per_chunk,)
+                            + model.shape[1:])
+
+# load data in in chunks and apply gains to each chunk
+xds = xds_from_ms(args.ms, columns=cols, chunks={"row": row_chunks})[0]
+vis = getattr(xds, args.data_col).data
+ant1 = xds.ANTENNA1.data
+ant2 = xds.ANTENNA2.data
+uvw = xds.UVW.data
+
+# apply gains
+corrupted_data = compute_and_corrupt_vis(tbin_idx, tbin_counts, ant1, ant2,
+                                         jones, model, uvw, freq, lm)
+
+if reshape_vis:
+    corrupted_data = corrupted_data.reshape(n_row, n_chan, n_corr)
+
+# Assign visibilities to args.out_col and write to ms
+data = xr.DataArray(corrupted_data, dims=["row", "chan", "corr"])
+
+xds = xds.assign(**{args.out_col: data})
+
+# Create a write to the table
+write = xds_to_table(xds, args.ms, [args.out_col])
+
+# Submit all graph computations in parallel
+with ProgressBar():
+    write.compute()
