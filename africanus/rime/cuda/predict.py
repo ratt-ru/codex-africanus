875c8cb2e6b8a8c18a14bf2390194f364748e678
Simon Perkins <simon.perkins@gmail.com>
2019-09-11 14:57:49 +0200

    Missing reduce

--- africanus/rime/cuda/predict.py
+++ africanus/rime/cuda/predict.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from functools import reduce
 import logging
 from operator import mul
 from os.path import join as pjoin
