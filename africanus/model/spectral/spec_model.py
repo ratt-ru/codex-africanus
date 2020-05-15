46a5335f04835ee873b7fc4b906738aa792ed195
Simon Perkins <simon.perkins@gmail.com>
2020-05-15 18:03:58 +0200

    Further fixes

--- africanus/model/spectral/spec_model.py
+++ africanus/model/spectral/spec_model.py
@@ -220,7 +220,7 @@ Compute a spectral model, per polarisation.
     :nowrap:
 
     \begin{eqnarray}
-    I(\lambda) & = & I_0 \prod_{i=1} (\lambda / \lambda_0 - 1)^{\alpha_{i}} \\
+    I(\lambda) & = & I_0 \prod_{i=1} (\lambda / \lambda_0)^{\alpha_{i}} \\
     \ln( I(\lambda) ) & = & \sum_{i=0} \alpha_{i}
       \ln (\lambda / \lambda_0)^i
       \, \textrm{where} \, \alpha_0 = \ln I_0 \\
