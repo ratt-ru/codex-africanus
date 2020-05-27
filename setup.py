99436549fbc3def8eafd8dc5e6cb5d9193d84916
Simon Perkins <simon.perkins@gmail.com>
2020-05-27 14:19:13 +0200

    Upgrade jax, scipy and python-casacore

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ if not on_rtd:
 extras_require = {
     'cuda': ['cupy >= 5.0.0', 'jinja2 >= 2.10'],
     'dask': ['dask[array] >= 1.1.0'],
-    'jax': ['jax == 0.1.27', 'jaxlib == 0.1.14'],
-    'scipy': ['scipy >= 1.0.0'],
+    'jax': ['jax == 0.1.68', 'jaxlib == 0.1.47'],
+    'scipy': ['scipy >= 1.4.0'],
     'astropy': ['astropy >= 3.0'],
-    'python-casacore': ['python-casacore >= 3.2.0'],
+    'python-casacore': ['python-casacore >= 3.3.1'],
     'testing': ['pytest', 'flaky', 'pytest-flake8 >= 1.0.6']
 }
 
