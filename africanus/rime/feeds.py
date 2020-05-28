9aea230ec7d3e1b2bf8452b49257641e2c09d535
Simon Perkins <simon.perkins@gmail.com>
2020-05-28 13:40:04 +0200

    Numbify feed_rotation

--- africanus/rime/feeds.py
+++ africanus/rime/feeds.py
@@ -7,69 +7,51 @@ from operator import mul
 import numpy as np
 
 from africanus.util.docs import DocstringTemplate
-from africanus.util.numba import jit
+from africanus.util.numba import generated_jit
 
 
-@jit(nopython=True, nogil=True, cache=True)
-def _nb_feed_rotation(parallactic_angles, feed_type, feed_rotation):
-    shape = parallactic_angles.shape
-    parangles = parallactic_angles.flat
-
-    # Linear feeds
-    if feed_type == 0:
-        for i, pa in enumerate(parangles):
-            pa_cos = np.cos(pa)
-            pa_sin = np.sin(pa)
-
-            feed_rotation.real[i, 0, 0] = pa_cos
-            feed_rotation.imag[i, 0, 0] = 0.0
-            feed_rotation.real[i, 0, 1] = pa_sin
-            feed_rotation.imag[i, 0, 1] = 0.0
-            feed_rotation.real[i, 1, 0] = -pa_sin
-            feed_rotation.imag[i, 1, 0] = 0.0
-            feed_rotation.real[i, 1, 1] = pa_cos
-            feed_rotation.imag[i, 1, 1] = 0.0
-
-    # Circular feeds
-    elif feed_type == 1:
-        for i, pa in enumerate(parangles):
-            pa_cos = np.cos(pa)
-            pa_sin = np.sin(pa)
-
-            feed_rotation.real[i, 0, 0] = pa_cos
-            feed_rotation.imag[i, 0, 0] = -pa_sin
-            feed_rotation[i, 0, 1] = 0.0 + 0.0*1j
-            feed_rotation[i, 1, 0] = 0.0 + 0.0*1j
-            feed_rotation.real[i, 1, 1] = pa_cos
-            feed_rotation.imag[i, 1, 1] = pa_sin
-    else:
-        raise ValueError("Invalid feed_type")
-
-    return feed_rotation.reshape(shape + (2, 2))
-
-
+@generated_jit(nopython=True, nogil=True, cache=True)
 def feed_rotation(parallactic_angles, feed_type='linear'):
-    if feed_type == 'linear':
-        poltype = 0
-    elif feed_type == 'circular':
-        poltype = 1
-    else:
-        raise ValueError("Invalid feed_type '%s'" % feed_type)
+    pa_np_dtype = np.dtype(parallactic_angles.dtype.name)
+    dtype = np.result_type(pa_np_dtype, np.complex64)
 
-    if parallactic_angles.dtype == np.float32:
-        dtype = np.complex64
-    elif parallactic_angles.dtype == np.float64:
-        dtype = np.complex128
-    else:
-        raise ValueError("parallactic_angles has "
-                         "none-floating point type %s"
-                         % parallactic_angles.dtype)
+    def impl(parallactic_angles, feed_type='linear'):
+        parangles = parallactic_angles.ravel()
+        # Can't prepend shape tuple till the following is fixed
+        # https://github.com/numba/numba/issues/5439
+        # We know parangles.ndim == 1 though
+        result = np.zeros((parangles.shape[0], 2, 2), dtype=dtype)
 
-    # Create result array with flattened parangles
-    shape = (reduce(mul, parallactic_angles.shape),) + (2, 2)
-    result = np.empty(shape, dtype=dtype)
+        # Linear feeds
+        if feed_type == 'linear':
+            for i in range(parangles.shape[0]):
+                pa = parangles[i]
+                pa_cos = np.cos(pa)
+                pa_sin = np.sin(pa)
 
-    return _nb_feed_rotation(parallactic_angles, poltype, result)
+                result[i, 0, 0] = pa_cos + 0j
+                result[i, 0, 1] = pa_sin + 0j
+                result[i, 1, 0] = -pa_sin + 0j
+                result[i, 1, 1] = pa_cos + 0j
+
+        # Circular feeds
+        elif feed_type == 'circular':
+            for i in range(parangles.shape[0]):
+                pa = parangles[i]
+                pa_cos = np.cos(pa)
+                pa_sin = np.sin(pa)
+
+                result[i, 0, 0] = pa_cos - pa_sin*1j
+                result[i, 0, 1] = 0.0
+                result[i, 1, 0] = 0.0
+                result[i, 1, 1] = pa_cos + pa_sin*1j
+        else:
+            raise ValueError("feed_type not in ('linear', 'circular')")
+
+        return result.reshape(parallactic_angles.shape + (2, 2))
+
+    return impl
+
 
 
 FEED_ROTATION_DOCS = DocstringTemplate(r"""
