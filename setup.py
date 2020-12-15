26861293dbfed893ebf2293988caa42b7d203f34
Simon Perkins <simon.perkins@gmail.com>
2020-12-15 11:03:04 +0200

    Bump version: 0.2.8 → 0.2.9

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ska-sa/codex-africanus',
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )
