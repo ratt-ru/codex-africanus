4b0cf6d26700fa4df3d3373bbe1e3f9b608ca21f
landmanbester <lbester@ska.ac.za>
2021-01-04 14:05:35 +0200

    Add mindet to avoid div0 errors in spi fitter

--- africanus/model/spi/component_spi.py
+++ africanus/model/spi/component_spi.py
@@ -10,7 +10,7 @@ from africanus.util.numba import jit
 
 @jit(nopython=True, nogil=True, cache=True)
 def _fit_spi_components_impl(data, weights, freqs, freq0, out,
-                             jac, ncomps, nfreqs, tol, maxiter):
+                             jac, ncomps, nfreqs, tol, maxiter, mindet):
     w = freqs/freq0
     for comp in range(ncomps):
         eps = 1.0
@@ -37,7 +37,7 @@ def _fit_spi_components_impl(data, weights, freqs, freq0, out,
                 hess00 += jac[0, v] * weights[v] * jac[0, v]
                 hess01 += jac[0, v] * weights[v] * jac[1, v]
                 hess11 += jac[1, v] * weights[v] * jac[1, v]
-            det = hess00 * hess11 - hess01**2
+            det = np.maximum(hess00 * hess11 - hess01**2, mindet)
             alphak = alphap + (hess11 * jr0 - hess01 * jr1)/det
             i0k = i0p + (-hess01 * jr0 + hess00 * jr1)/det
             eps = np.maximum(np.abs(alphak - alphap), np.abs(i0k - i0p))
@@ -67,8 +67,15 @@ def fit_spi_components(data, weights, freqs, freq0,
         tmp = np.abs(freqs - freq0)
         ref_freq_idx = np.argwhere(tmp == tmp.min()).squeeze()
         out[2, :] = data[:, ref_freq_idx]
+    if data.dtype == np.float64:
+        mindet = 1e-12
+    elif data.dtype == np.float32:
+        mindet = 1e-5
+    else:
+        raise ValueError("Unsupported data type. Must be float32 of float64.")
+
     return _fit_spi_components_impl(data, weights, freqs, freq0, out,
-                                    jac, ncomps, nfreqs, tol, maxiter)
+                                    jac, ncomps, nfreqs, tol, maxiter, mindet)
 
 
 SPI_DOCSTRING = DocstringTemplate(
