03e3fa876cb442d5244edba5b2a4c7031fc47e5e
Joshua van Staden <joshuavanstaden@pop-os.localdomain>
2021-01-29 08:01:33 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -24,14 +24,14 @@ if not on_rtd:
     ]
 
 extras_require = {
-    'cuda': ['cupy >= 5.0.0', 'jinja2 >= 2.10'],
-    'dask': ['dask[array] >= 1.1.0'],
-    'jax': ['jax == 0.1.68', 'jaxlib == 0.1.47'],
-    'scipy': ['scipy >= 1.4.0'],
-    'astropy': ['astropy >= 3.0'],
-    'python-casacore': ['python-casacore >= 3.3.1'],
-    'ducc0': ['ducc0 >= 0.8.0'],
-    'testing': ['pytest', 'flaky', 'pytest-flake8 >= 1.0.6']
+    "cuda": ["cupy >= 5.0.0", "jinja2 >= 2.10"],
+    "dask": ["dask[array] >= 1.1.0"],
+    "jax": ["jax == 0.1.68", "jaxlib == 0.1.47"],
+    "scipy": ["scipy >= 1.4.0"],
+    "astropy": ["astropy >= 3.0"],
+    "python-casacore": ["python-casacore >= 3.3.1"],
+    "ducc0": ["ducc0 >= 0.6.0"],
+    "testing": ["pytest", "flaky", "pytest-flake8 >= 1.0.6"],
 }
 
 with open(str(Path("africanus", "install", "extras_require.py")), "w") as f:
