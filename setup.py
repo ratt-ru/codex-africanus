57c58d94ec9ae569cdb83be8f983d5ef9704cc8e
Simon Perkins <simon.perkins@gmail.com>
2021-05-11 10:28:10 +0200

    Upgrade dask, python-casacore

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ if not on_rtd:
 
 extras_require = {
     'cuda': ['cupy >= 5.0.0', 'jinja2 >= 2.10'],
-    'dask': ['dask[array] >= 1.1.0'],
+    'dask': ['dask[array] >= 2.2.0'],
     'jax': ['jax >= 0.2.11', 'jaxlib >= 0.1.65'],
     'scipy': ['scipy >= 1.4.0'],
     'astropy': ['astropy >= 3.0'],
-    'python-casacore': ['python-casacore >= 3.3.1'],
+    'python-casacore': ['python-casacore >= 3.4.0'],
     'ducc0': ['ducc0 >= 0.9.0'],
     'testing': ['pytest', 'flaky', 'pytest-flake8 >= 1.0.6']
 }
