e6918f11066d359f45a371064fffcbb26e5f86fb
Simon Perkins <simon.perkins@gmail.com>
2021-08-25 21:46:52 +0200

    intrinsic for directly setting attributes on new StructRef

---
+++ africanus/rime/monolothic/tests/test_structref_setter.py
@@ -0,0 +1,74 @@
+import numba
+from numba.experimental import structref
+from numba.extending import intrinsic
+from numba.core import types, errors
+from numba.core.runtime import rtsys
+
+import numpy as np
+
+import pytest
+
+@structref.register
+class StateStructRef(types.StructRef):
+    def preprocess_fields(self, fields):
+        """ Disallow literal types in field definitions """
+        return tuple((n, types.unliteral(t)) for n, t in fields)
+
+
+@pytest.fixture
+def check_allocations():
+    """ Check allocations match frees """
+    try:
+        yield
+    finally:
+        stats = rtsys.get_allocation_stats()
+        assert stats.alloc == stats.free
+
+
+def test_structref_setter(check_allocations):
+    @intrinsic
+    def constructor(typingctx, arg_tuple):
+        if not isinstance(arg_tuple, types.BaseTuple):
+            raise errors.TypingError(f"{arg_tuple} must be a Tuple")
+
+        names = [f"arg_{i}" for i in range(len(arg_tuple))]
+        fields = list((name, typ) for name, typ in zip(names, arg_tuple))
+        state_type = StateStructRef(fields)
+
+        sig = state_type(arg_tuple)
+
+        def codegen(context, builder, signature, args):
+
+            def make_struct():
+                return structref.new(state_type)
+
+            U = structref._Utils(context, builder, state_type)
+
+            state = context.compile_internal(builder, make_struct,
+                                             state_type(), [])
+            # model = context.data_model_manager[state_type]
+            data_struct = U.get_data_struct(state)
+
+            for i, name in enumerate(names):
+                value = builder.extract_value(args[0], i)
+                value_type = signature.args[0][i]
+                field_type = state_type.field_dict[name]
+                casted = context.cast(builder, value,
+                                      value_type, field_type)
+                old_value = getattr(data_struct, name)
+                context.nrt.incref(builder, value_type, casted)
+                context.nrt.decref(builder, value_type, old_value)
+                setattr(data_struct, name, casted)
+
+            return state
+
+        return sig, codegen
+
+    @numba.njit
+    def fn(*args):
+        s = constructor(args)
+        print(s.arg_0)
+        print(s.arg_1)
+        print(s.arg_2)
+
+    fn(2, "bob", np.arange(10))
