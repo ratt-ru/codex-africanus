2af5bd6c8ef1b4987828fe2203b9e79cff8f8765
Simon Perkins <simon.perkins@gmail.com>
2021-10-22 23:44:54 +0200

    Construct argument+transformer graph and topologically sort it

--- africanus/rime/monolithic/tests/test_arg_graph.py
+++ africanus/rime/monolithic/tests/test_arg_graph.py
@@ -1,93 +1,138 @@
+from abc import abstractmethod, ABC
+
+
 def test_arg_graph():
-    class Edge:
-        def __init__(self, u, v, arg):
-            assert isinstance(arg, str)
-            self.arg = arg
-            self.u = u
-            self.v = v
 
-        def __str__(self):
-            return self.arg
+    class Node(ABC):
+        @property
+        @abstractmethod
+        def outputs(self):
+            raise NotImplementedError
 
-        def __repr__(self):
-            return f"Edge({self.u} -> {self.arg} -> {self.v})"
+    class Argument(Node):
+        def __init__(self, name, outputs=None):
+            self.name = name
+            self._outputs = set(outputs) if outputs else set()
 
-    class Node:
-        def __init__(self, inputs=(), outputs=()):
-            self.inputs = (tuple(inputs)
-                           if isinstance(inputs, (list, tuple))
-                           else (inputs,))
-            self.outputs = (tuple(outputs)
-                            if isinstance(outputs, (list, tuple))
-                            else (outputs,))
-
-        def __str__(self):
-            return f"{list(self.inputs)}: {list(self.outputs)}"
-
-        __repr__ = __str__
-
-    from pprint import pprint
-
-    class Term:
-        def __repr__(self):
-            return self.__class__.__name__
-
-    class BrightnessTerm(Term):
-        ARGS = ["chan_freq", "stokes"]
-
-    class PhaseTerm(Term):
-        ARGS = ["lm", "uvw", "chan_freq"]
-
-    class ArgPack:
-        def __init__(self, *args):
-            self.args = args
-
-        def __contains__(self, arg):
-            return arg in self.args
+        def add_output(self, output):
+            self._outputs.add(output)
 
         def __repr__(self):
-            return "ArgPack"
+            return self.name
 
         __str__ = __repr__
 
-    class Transformer:
-        def __init__(self, inputs, outputs):
-            self.inputs = set(inputs) if inputs else set()
-            self.outputs = set(outputs) if outputs else set()
+        @property
+        def outputs(self):
+            return self._outputs
+
+    class Term(Node):
+        @property
+        def outputs(self):
+            return ()
 
         def __repr__(self):
-            return "Transformer"
+            return self.__class__.__name__
 
-    argpack = ArgPack("phase_centre", "radec", "uvw",
-                      "chan_freq", "stokes", "spi")
-    terms = [PhaseTerm(), BrightnessTerm()]
-    graph = [Edge(argpack, term, a) for term in terms for a in term.ARGS]
+        __str__ = __repr__
 
-    in_outs = [
-        [["phase_centre", "radec"], ["lm"]],
+    class BrightnessTerm(Term):
+        inputs = ["chan_freq", "stokes"]
+
+    class PhaseTerm(Term):
+        inputs = ["lm", "uvw", "chan_freq"]
+
+    class Transformer(Node):
+        def __init__(self, name, inputs, outputs):
+            self.name = name
+            self.inputs = set(inputs)
+            self._outputs = set(Argument(o) for o in outputs)
+
+        def add_outputs(self, output):
+            self._outputs.add(output)
+
+        @property
+        def outputs(self):
+            return self._outputs
+        
+        def __repr__(self):
+            return self.name
+
+        __str__ = __repr__
+
+
+    terms = [BrightnessTerm(), PhaseTerm()]
+    args = {"phase_centre", "radec", "uvw",
+            "chan_freq", "stokes", "spi"}
+    variables = {n: Argument(n) for n in args}
+
+    xformer_args = [
+        ["RadecTransformer", ["phase_centre", "radec"], ["lm"]],
     ]
-
-    xformers = {o: Transformer(ins, outs)
-                for ins, outs in in_outs
-                for o in outs}
-
-    graph = []
+    xformers = [Transformer(n, i, o) for n, i, o in xformer_args]
+    xformers = {o.name: t for t in xformers for o in t.outputs}
 
     for term in terms:
-        for a in term.ARGS:
-            if a not in argpack:
+        for a in term.inputs:
+            try:
+                # Try obtain the argument from existing variables
+                arg = variables[a]
+            except KeyError:
+                # Doesn't exist, attempt to find a transformer that
+                # produces it
                 try:
                     xformer = xformers[a]
                 except KeyError:
-                    raise ValueError(f"{a} not in {argpack} "
-                                     f"and no transformers available")
+                    raise ValueError(f"'{a}' not in {list(variables.keys())} "
+                                     f"and no transformers exist"
+                                     f"to create it")
 
+                # The transformer inputs must be present in variables
                 for input in xformer.inputs:
-                    graph.append(Edge(argpack, xformer, input))
+                    try:
+                        arg = variables[input]
+                    except KeyError:
+                        raise ValueError(f"{xformer} needs '{input}' to "
+                                         f"produce '{a}' but '{input}' is not "
+                                         f"present in the supplied "
+                                          f"arguments")
+                    else:
+                        # Add the transformer as the argument's neighbour
+                        arg.add_output(xformer)
 
-                for output in xformer.outputs:
-                    graph.append(Edge(xformer, term, output))
+                output = next(o for o in xformer.outputs if o.name == a)
+                output.add_output(term)
             else:
-                graph.append(Edge(argpack, term, a))
+                arg.add_output(term)
 
-    pprint(graph)
+
+    def toposort(graph):
+        stack = []
+        visited = set()
+
+        def _recurse(node):
+            if node in visited:
+                return
+
+            visited.add(node)
+            has_outputs = False
+
+            for o in node.outputs:
+                _recurse(o)
+                has_outputs = True
+
+            # Don't visit argument's with no outputs
+            if isinstance(node, Argument) and not has_outputs:
+                return
+
+            stack.append(node)
+
+        for arg in graph:
+            _recurse(arg)
+
+        return stack[::-1]
+
+    order = toposort(variables.values())
+    print(order)
+
+
