c7df6e9a3e08e22aac92438091dbad3f097d870a
Simon Perkins <simon.perkins@gmail.com>
2021-12-03 16:06:34 +0200

    flake8

--- africanus/rime/fused/__init__.py
+++ africanus/rime/fused/__init__.py
@@ -1,2 +1,2 @@
-from africanus.rime.fused.specification import RimeSpecification
-from africanus.rime.fused.core import rime
+from africanus.rime.fused.specification import RimeSpecification  # noqa
+from africanus.rime.fused.core import rime  # noqa
