0d95b7e39d5891bf551acaa49fd247286455281c
Simon Perkins <simon.perkins@gmail.com>
2024-10-31 13:02:13 +0200

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -19,13 +19,6 @@
 # absolute, like shown here.
 #
 
-import importlib
-import os
-import sys
-
-import sphinx_rtd_theme
-import africanus
-
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -94,7 +87,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata-sphinx-theme"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
