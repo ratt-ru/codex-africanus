e50b588afc09b8e2e08a2a56018fa8f41652160c
Simon Perkins <simon.perkins@gmail.com>
2024-10-31 09:35:59 +0200

    Revert "Add setuptools to setup_requirements"
    This reverts commit 04a5e39fd61475028bb1e5a1ff567f2e21e56251.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extras_require["complete"] = sorted(set(sum(_non_cuda_extras, [])))
 extras_require["complete-cuda"] = sorted(set(sum(_all_extras, [])))
 
 
-setup_requirements = ["setuptools"]
+setup_requirements = []
 test_requirements = (
     extras_require["testing"]
     + extras_require["astropy"]
